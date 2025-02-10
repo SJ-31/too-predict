f4ee5971d11fe5ff4e1ffd32c1bfe558ec713478
Shann Chongwattanankul <shanj3131@gmail.com>
2025-02-10 17:35:52 +0700

    feat: add utils for retrieving data and R scripts

--- src/too_predict/utils.py
+++ src/too_predict/utils.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env ipython
 
+import importlib.resources as res
+from pathlib import Path
 from typing import Callable
 
 import anndata as ad
 import pandas as pd
 import rpy2.robjects as ro
 from rpy2.robjects import RObject, pandas2ri
-from rpy2.robjects.packages import importr
+from rpy2.robjects.packages import STAP, importr
 from scipy import sparse
 
+import too_predict
+
+# R libraries
 base = None
+ensembldb = None
+
+
+def get_data(path: str) -> Path:
+    """Retrieve the path of a file in this package's `data` directory
+    :param: path relative path to the desired file
+    """
+    with res.path(too_predict) as root:
+        file = root.parent.parent.joinpath("data").joinpath(path)
+        if file.exists():
+            return file.absolute()
+        raise FileNotFoundError(f"{path} doesn't exist!")
 
 
 def df_from_r(robj) -> pd.DataFrame:
@@ -47,3 +64,14 @@ def dgelist2anndata(rds: str | RObject) -> ad.AnnData:
         )
         adata.var.index = adata.var.iloc[:, 0]
     return adata
+
+
+def library(r_script: str) -> STAP:
+    """Import `r script` in src/R as a STAP"""
+    with res.path(too_predict) as root:
+        r_src = root.parent.joinpath("R")
+        script = r_src.joinpath(r_script)
+        if script.exists():
+            text = script.read_text()
+            return STAP(text, Path(r_script).stem)
+        raise FileNotFoundError(f"{r_script} doesn't exist in src/R!")
