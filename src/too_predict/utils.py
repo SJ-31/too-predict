5a76e5c7607f692961acf25c8c8020f0b947c54c
Shann Chongwattanankul <shanj3131@gmail.com>
2025-02-11 16:53:11 +0700

    feat: add gene annotation function

--- src/too_predict/utils.py
+++ src/too_predict/utils.py
@@ -5,8 +5,17 @@ from pathlib import Path
 from typing import Callable
 
 import anndata as ad
+import numpy as np
 import pandas as pd
 import rpy2.robjects as ro
+from rpy2 import rinterface, rinterface_lib
+from rpy2.rinterface_lib.sexp import (
+    NACharacterType,
+    NAComplexType,
+    NAIntegerType,
+    NALogicalType,
+    NARealType,
+)
 from rpy2.robjects import RObject, pandas2ri
 from rpy2.robjects.packages import STAP, importr
 from scipy import sparse
@@ -16,6 +25,14 @@ import too_predict
 # R libraries
 base = None
 ensembldb = None
+
+NA_TYPES: set = {
+    NACharacterType,
+    NAIntegerType,
+    NALogicalType,
+    NARealType,
+    NAComplexType,
+}
 
 
 def get_data(path: str) -> Path:
@@ -31,7 +48,10 @@ def get_data(path: str) -> Path:
 
 def df_from_r(robj) -> pd.DataFrame:
     with (ro.default_converter + pandas2ri.converter).context():
-        return ro.conversion.get_conversion().rpy2py(robj)
+        converted = ro.conversion.get_conversion().rpy2py(robj)
+        # BUG <2025-02-11 Tue>: rpy2 doesn't convert R NAs correctly
+        df = converted.map(lambda x: np.nan if type(x) in NA_TYPES else x)
+        return df
 
 
 def r_cleanup(fn: Callable):
@@ -75,3 +95,38 @@ def library(r_script: str) -> STAP:
             text = script.read_text()
             return STAP(text, Path(r_script).stem)
         raise FileNotFoundError(f"{r_script} doesn't exist in src/R!")
+
+
+@r_cleanup
+def add_gene_metadata(
+    adata: ad.AnnData,
+    keycol: str = "",
+    columns=("GENENAME", "GENEID", "GENEBIOTYPE", "SEQNAME"),
+    ensdb_path: str = "",
+    keytpe: str = "GENEID",
+) -> None:
+    """Add gene metadata from the ensembldb object stored at `ensdb_path`"""
+    for c in columns:
+        if c in adata.var.columns:
+            print(f"WARNING: column {c} already exists in adata.var, removing...")
+            adata.var = adata.var.drop(c, index="columns")
+    if not ensdb_path:
+        ensdb_path = str(get_data("Homo_sapiens.GRCh38.113.sqlite"))
+    ro.globalenv["db"] = ensembldb.EnsDb(ensdb_path)
+    ro.globalenv["cols"] = ro.StrVector(columns)
+    if keycol:
+        keys = adata.var[keycol]
+    else:
+        keys = adata.var.index
+    ro.globalenv["keys"] = ro.StrVector(keys)
+    ro.globalenv["anno"] = ro.r(
+        f"AnnotationDbi::select(db, keys = keys, columns = cols, keytype = '{keytpe}')"
+    )
+    result: pd.DataFrame = df_from_r(ro.r("anno"))
+    if keycol:
+        result[keycol] = ro.StrVector(result[keytpe])
+    else:
+        result.index = ro.StrVector(result[keytpe])
+
+    join_on = keycol if keycol else None
+    adata.var = adata.var.join(result, on=join_on, how="left")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env ipython
 
-import importlib
+import importlib.resources as res
+from pathlib import Path
 
 import anndata as ad
+import pandas as pd
 import rpy2.robjects as ro
+import too_predict
 from pyhere import here
+from rpy2.rinterface import SexpVector
+from rpy2.robjects import default_converter, numpy2ri
 from rpy2.robjects.packages import importr
 from scipy import sparse
-from too_predict.utils import df_from_r, dgelist2anndata, r_cleanup
+from too_predict.utils import (
+    add_gene_metadata,
+    df_from_r,
+    dgelist2anndata,
+    get_data,
+    r_cleanup,
+)
 
 base = importr("base")
+ensembldb = importr("ensembldb")
 datadir = here("data", "tests")
 hcc = here(datadir, "tcga_hcc.rds")
 
-
 adata = dgelist2anndata(str(hcc))
+
+hg38 = here("data", "Homo_sapiens.GRCh38.113.sqlite")
+
+adata: ad.AnnData = dgelist2anndata(str(hcc))
+adata.var.index = adata.var.index.to_series().str.replace("\\..*", "", regex=True)
+add_gene_metadata(adata)
