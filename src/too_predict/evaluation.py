2be4448a375d6186299b2a83d1693b18a42c5f07
Shann Chongwattanankul <shanj3131@gmail.com>
2025-02-14 09:49:15 +0700

    chore: refactor evaluation into new file, add curve calculators

---
+++ src/too_predict/evaluation.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env ipython
+from typing import Callable
+
+import numpy as np
+import pandas as pd
+import sklearn.metrics as me
+
+
+def curve_multiclass(
+    curve_fn: Callable,
+    first: str,
+    second: str,
+    true_labels,
+    pred_proba: pd.DataFrame | np.ndarray,
+    summary_fn: Callable = None,
+    summary_fn_name: str = "",
+    classes=None,
+):
+    """
+    Higher-order function for computing binary curve metrics e.g. ROC, precision-recall
+    for a multi-class classification task
+    using the One-vs-Rest (OvR) approach and return them in a pandas DataFrame.
+
+    Parameters:
+    -----------
+    true_labels : np.ndarray | pd.Series | list
+        The true class labels for each sample.
+
+    pred_proba : np.ndarray | pd.DataFrame
+        The predicted probabilities for each class, where each column corresponds to a class.
+
+    curve_fn : callable function with signature Callable[true, score, pos_label] -> first, second, thresholds
+        which are ndarrays
+
+    classes : If pred_proba are the array the predicted probabilities,
+        the names of the classes in their order of appearance
+
+    summary_fn : Callable[true, score, labels]
+
+    Returns:
+    --------
+    pd.DataFrame
+        A DataFrame containing:
+        - x_axis (float)
+        - y_axis (float)
+        - 'thresholds' (float): The decision thresholds.
+        - 'class' (str): The class for which the metrics metrics are computed.
+        - summary_fn_name: Summary metric for the current class, if provided
+    """
+    if not isinstance(pred_proba, pd.DataFrame) and classes:
+        prob_df = pd.DataFrame(pred_proba, columns=classes)
+    elif not isinstance(pred_proba, pd.DataFrame) and not classes:
+        raise ValueError("If `pred_proba` isn't a df, the classes must be provided!")
+    else:
+        prob_df = pred_proba
+    summary_labels = {}
+    if summary_fn:
+        summary = summary_fn(true_labels, prob_df, prob_df.columns)
+        summary_labels: dict = {
+            c: f"{c}: {v.round(3)}" for c, v in zip(prob_df.columns, summary)
+        }
+    dfs = []
+    for c in prob_df.columns:
+        f, s, thresholds = curve_fn(true_labels, prob_df[c], c)
+        if len(thresholds) < len(f):
+            thresholds = np.concatenate([[np.inf], thresholds])
+        tmp = pd.DataFrame({first: f, second: s, "thresholds": thresholds})
+        tmp["class"] = c
+        if summary_fn and summary_fn_name:
+            tmp[summary_fn_name] = summary_labels[c]
+        dfs.append(tmp)
+    roc_df = pd.concat(dfs)
+    return roc_df
+
+
+def roc_multiclass(true_labels, pred_proba: pd.DataFrame | np.ndarray, classes=None):
+    return curve_multiclass(
+        lambda true, score, labels: me.roc_curve(true, score, pos_label=labels),
+        "fpr",
+        "tpr",
+        true_labels,
+        pred_proba,
+        classes=classes,
+        summary_fn=lambda true, score, labels: me.roc_auc_score(
+            true, score, average=None, multi_class="ovr", labels=labels
+        ),
+        summary_fn_name="class_auc",
+    )
+
+
+def precision_recall_multiclass(
+    true_labels, pred_proba: pd.DataFrame | np.ndarray, classes=None
+):
+    return curve_multiclass(
+        lambda true, score, labels: me.precision_recall_curve(
+            true, score, pos_label=labels
+        ),
+        "precision",
+        "recall",
+        true_labels,
+        pred_proba,
+        classes=classes,
+        summary_fn=lambda true, score, _: me.average_precision_score(
+            true, score, average=None
+        ),
+        summary_fn_name="average_precision",
+    )
+
+
+def classification_report2df(
+    report: dict, fold: int = None
+) -> tuple[pd.DataFrame, float]:
+    """Convert scikit-learn classification report (dict format) into a long dataframe
+    :return: tuple[datframe of the major metrics, accuracy]
+    """
+    dct = {"class": [], "precision": [], "recall": [], "f1-score": [], "support": []}
+    metrics = set(dct.keys()) - {"class"}
+    for c in report.keys():
+        if c == "accuracy":
+            continue
+        dct["class"].append(c)
+        for r in metrics:
+            dct[r].append(report[c][r])
+    df = pd.DataFrame(dct)
+    if fold:
+        df["fold"] = fold
+    return df, report["accuracy"]
