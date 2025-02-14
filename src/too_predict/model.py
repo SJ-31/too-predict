7cd39c35d4e0d21d11a65560c1cbb698521ace2e
Shann Chongwattanankul <shanj3131@gmail.com>
2025-02-14 11:44:21 +0700

    chore: make helper methods for model interaction

--- src/too_predict/model.py
+++ src/too_predict/model.py
@@ -7,7 +7,6 @@ from typing import Callable
 import anndata as ad
 import numpy as np
 import pandas as pd
-import rpy2.robjects as ro
 import sklearn.model_selection as ms
 from sklearn.ensemble import RandomForestClassifier
 
@@ -39,50 +38,60 @@ class Base:
             self.n_method = normalization.lower()
         else:
             self.n_method = None
+        self.features = features
         self.impute: Callable = Imputer(imputation).run
         self.i_method = imputation.lower()
 
-    def fit(self, X: ad.AnnData, label_col="tumor_type") -> None:
-        N = self.normalize(X)
-        self.model.fit(N.X, N.obs[label_col])
+    def fit(self, X: ad.AnnData, label_col="tumor_type", preprocess=True) -> None:
+        if preprocess:
+            X = self.preprocess(X)
+        self.model.fit(X.X, X.obs[label_col])
+
+    def predict_proba(self, X: ad.AnnData, preprocess=True) -> np.ndarray:
+        if preprocess:
+            X = self.preprocess(X)
+        return self.model.predict_proba(X.X)
 
     def load(self, path: str) -> None:
         """Load the fitted estimator from the saved path"""
         self.model = pickle.load()
-        pass
 
-    def predict(self, X: ad.AnnData) -> np.ndarray:
-        N = self.normalize(X)
+    def predict(self, X: ad.AnnData, preprocess: bool = True) -> np.ndarray:
+        if preprocess:
+            N = self.preprocess(X)
         return self.model.predict(N)
 
-    def normalize(self, adata: ad.AnnData) -> ad.AnnData:
+    def preprocess(self, adata: ad.AnnData) -> ad.AnnData:
+        if self.features is not None:
+            adata = adata[:, self.features]
         if self.n_method is not None:
             normalized: ad.AnnData = Normalizer(
                 adata, self.n_method, impute_fn=self.impute, inplace=False
             ).run()
             gc.collect()
             return normalized
-        print("WARNING: Using raw counts")
         return adata.copy()
+
+    def _classes():
+        return self.model.classes_
 
     def cross_validate(self, adata, cv=None, label_col="tumor_type") -> dict:
         """Determine model performance with cross-validation"""
         if not cv:
             cv = ms.StratifiedKFold(n_splits=5)
-        N = self.normalize(adata)
+        N = self.preprocess(adata)
         cm: dict = {}
         roc, prec_recall, report = [], [], []
         accs: dict = {"fold": [], "acc": []}
         for fold, (train_i, test_i) in enumerate(cv.split(N.X, N.obs[label_col])):
-            x_train = N[train_i].X
-            y_train = N.obs[label_col].iloc[train_i]
-            self.model.fit(x_train, y=y_train)
+            x_train = N[train_i]
+            self.fit(x_train, preprocess=False)
 
-            x_test = N[test_i].X
+            x_test = N[test_i]
             y_true = N.obs[label_col].iloc[test_i]  # True values
 
-            proba = self.model.predict_proba(x_test)
-            res: dict = get_all_metrics(y_true, proba, self.model.classes_)
+            proba = self.predict_proba(x_test, preprocess=False)
+            res: dict = get_all_metrics(y_true, proba, self._classes())
             accs["fold"].append(fold)
             accs["acc"].append(res["acc"])
             cm[fold] = res["cm"]
@@ -99,6 +108,16 @@ class Base:
             "prec_recall": pd.concat(prec_recall),
             "roc": pd.concat(roc),
         }
+
+
+class TooRandomForest(Base):
+    def __init__(self, normalization: str, imputation: str) -> None:
+        super().__init__(
+            normalization=normalization,
+            imputation=imputation,
+            model=RandomForestClassifier(),
+            features=None,
+        )
 
 
 # TODO: going to need a meta model or something to implement the ALR normalization properly
