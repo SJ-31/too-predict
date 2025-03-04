31abf1ea44f9d066c219b5f461e303bb0e9e914d
Shann Chongwattanankul <shanj3131@gmail.com>
2025-03-04 15:20:47 +0700

    chore: add heatmap plot to normalization comparison

--- scripts/compare_normalization.py
+++ scripts/compare_normalization.py
@@ -7,6 +7,7 @@ import anndata as ad
 import joblib
 import numpy as np
 import pandas as pd
+import rpy2.robjects as ro
 import scanpy as sc
 import sklearn.metrics as sm
 from dask.distributed import Client
@@ -15,27 +16,30 @@ from pyhere import here
 from too_predict.imputer import IMPLEMENTED_IMPUTATION, Imputer
 from too_predict.normalizer import IMPLEMENTED_NORMALIZATION, Normalizer
 from too_predict.utils import (
+    adata_x_to_r,
+    df_to_r,
     read_existing,
-    take_from_ad,
+    source,
     training_data_internal,
     training_data_internal_test,
 )
 
-logger = logging.getLogger()
+LOGGER = logging.getLogger()
 
 DATADIR = here("data", "tests")
 OUTDIR = here("data", "output", "normalization_comparison")
+FS_DIR = here("data", "output", "feature_selection")
 STORAGE_DIR = here("remote", "repos", "too-predict", "normalization_comparison")
 logging.basicConfig(filename=here(OUTDIR, "log"))
-failed_tracker = {"normalization": [], "imputation": [], "reason": []}
+FAILED_TRACKER = {"normalization": [], "imputation": [], "reason": []}
 
 
-def helper(adata, i: str, n: str, **kwargs) -> ad.AnnData | None:
+def helper(feature_set, adata, i: str, n: str, **kwargs) -> ad.AnnData | None:
     if i == "labelled_median":
         impute_fn = lambda x: Imputer(i).run(x, labels=adata.obs["tumor_type"])
     else:
         impute_fn = Imputer(i).run
-    output = here(STORAGE_DIR, f"{n}-{i}.h5ad")
+    output = here(STORAGE_DIR, feature_set, f"{n}-{i}.h5ad")
     if not output.exists():
         normalized: ad.AnnData = Normalizer(
             adata,
@@ -50,11 +54,11 @@ def helper(adata, i: str, n: str, **kwargs) -> ad.AnnData | None:
                 del normalized.layers["counts"]
             normalized.write_h5ad(output)
         except ValueError as e:
-            logger.error(f"ValueError with imputation {i} and normalization {n}")
-            failed_tracker["imputation"].append(i)
-            failed_tracker["normalization"].append(n)
-            failed_tracker["reason"].append(e)
-            logger.error(e)
+            LOGGER.error(f"ValueError with imputation {i} and normalization {n}")
+            FAILED_TRACKER["imputation"].append(i)
+            FAILED_TRACKER["normalization"].append(n)
+            FAILED_TRACKER["reason"].append(e)
+            LOGGER.error(e)
     else:
         adata = ad.read_h5ad(output)
         if "neighbors" not in adata.uns:
@@ -84,20 +88,26 @@ def parse_args():
     return parser.parse_args()
 
 
-var_explained: list = []
+VAR_EXPLAINED: list = []
 NORMALIZATION_METHODS = IMPLEMENTED_NORMALIZATION
-METRIC_OUTPUT: pd.DataFrame = pd.read_csv(here(OUTDIR, f"{DATE}-label_matrics.csv"))
+IMPUTATION_METHODS = ["plus_one"]
+METRIC_OUTPUT = here(OUTDIR, f"{DATE}-label_metrics.csv")
 
-# Adding selected features for CLR
 REF_LISTS: dict = {}
-for ref_list in here(
-    "data", "output", "feature_selection", "reference_lists"
-).iterdir():
-    with open(ref_list, "r") as f:
-        features = f.read().strip().splitlines()
-    name = f"clr_{ref_list.stem}"
-    NORMALIZATION_METHODS.append(name)
-    REF_LISTS[name] = features
+# Adding selected features for CLR
+FEATURE_LISTS: dict = {}
+
+for fname, add_to in zip(
+    [here(FS_DIR, "feature_lists"), here(FS_DIR, "reference_lists")],
+    [FEATURE_LISTS, REF_LISTS],
+):
+    for file in fname.iterdir():
+        with open(file, "r") as f:
+            items = f.read().strip().splitlines()
+        name = file.stem
+        add_to[name] = items
+
+FEATURE_LISTS["all_features"] = None
 
 
 def get_metrics(adata: ad.AnnData, label, normalization, imputation, f):
@@ -113,8 +123,10 @@ def get_metrics(adata: ad.AnnData, label, normalization, imputation, f):
     return df
 
 
-def main(adata):
-    for i in IMPLEMENTED_IMPUTATION:
+def main(adata, feature_set_name):
+    outdir = here(OUTDIR, feature_set_name)
+    outdir.mkdir(exist_ok=True)
+    for i in IMPUTATION_METHODS:
         for n in NORMALIZATION_METHODS:
             if "alr" in n or i is None:
                 continue
@@ -123,18 +135,20 @@ def main(adata):
                     adata, i=i, n="clr", kwargs={"features": REF_LISTS[n]}
                 )
             else:
-                normalized = helper(adata, i=i, n=n, kwargs={})
+                normalized = helper(
+                    feature_set=feature_set_name, adata=adata, i=i, n=n, kwargs={}
+                )
             if normalized is not None:
                 for v in VARS:
-                    if not ((var_dir := here(OUTDIR, v)).exists()):
+                    if not ((var_dir := here(outdir, v)).exists()):
                         var_dir.mkdir(exist_ok=True)
-                    var_explained.append(
+                    VAR_EXPLAINED.append(
                         [[n, i] + list(normalized.uns["pca"]["variance_ratio"])]
                     )
                     filename = here(var_dir, f"{DATE}-{i}_{n}_pca.png")
                     u_filename = here(var_dir, f"{DATE}-{i}_{n}_umap.png")
 
-                    current_metrics_file = here(OUTDIR, f".{n}_{i}_{v}_metrics.csv")
+                    current_metrics_file = here(outdir, f".{n}_{i}_{v}_metrics.csv")
                     metric_df = read_existing(
                         current_metrics_file,
                         lambda f: get_metrics(
@@ -144,20 +158,35 @@ def main(adata):
                     )
 
                     ALL_METRICS.append(metric_df)
-                    fig = sc.pl.pca(normalized, color=v, return_fig=True)
+                    colors = [v, "Project_ID", "Sample_Type"]
+                    fig = sc.pl.pca(normalized, color=colors, return_fig=True)
                     fig.set_size_inches(10, 10)
                     fig.savefig(filename, dpi=500, bbox_inches="tight")
 
-                    u_fig = sc.pl.umap(normalized, color=v, return_fig=True)
+                    u_fig = sc.pl.umap(normalized, color=colors, return_fig=True)
                     u_fig.set_size_inches(10, 10)
                     u_fig.savefig(u_filename, dpi=500, bbox_inches="tight")
 
-        all_df = pd.concat(ALL_METRICS, ignore_index=True)
-        var_df = pd.DataFrame(var_explained)
-        failed_df = pd.DataFrame(failed_tracker)
+                    if feature_set_name != "all_features":
+                        source("plotting.R", True)
+                        df_to_r(normalized.obs, r_symbol="obs")
+                        adata_x_to_r(normalized, "counts")
+                        heatmap_file = here(var_dir, f"{DATE}-{i}_{n}_heatmap.png")
+                        fncall = f"""
+                        pheatmap_helper(obs=obs, counts=counts,
+                            sample_annotations = list(tumor_type = NULL, Sample_Type = NULL),
+                            order_on = 'tumor_type',
+                            pheatmap_kwargs = list(file = '{heatmap_file}'))
+                        """
+                        ro.r(fncall)
+                        pass
 
-        failed_df.to_csv(here(OUTDIR, f"{DATE}-failed.csv"), index=False)
-        var_df.to_csv(here(OUTDIR, f"{DATE}-variance_explained.csv"), index=False)
+        all_df = pd.concat(ALL_METRICS, ignore_index=True)
+        var_df = pd.DataFrame(VAR_EXPLAINED)
+        failed_df = pd.DataFrame(FAILED_TRACKER)
+
+        failed_df.to_csv(here(outdir, f"{DATE}-failed.csv"), index=False)
+        var_df.to_csv(here(outdir, f"{DATE}-variance_explained.csv"), index=False)
         all_df.to_csv(METRIC_OUTPUT, index=False)
 
 
@@ -170,11 +199,18 @@ if __name__ == "__main__":
         OUTDIR.mkdir(exist_ok=True, parents=True)
     else:
         adata = training_data_internal()
-    cluster = SLURMCluster(cores=args.cores, memory=f"{args.memory} GB")
-    client = Client(cluster)
+    # cluster = SLURMCluster(cores=args.cores, memory=f"{args.memory} GB", queue="low")
+    # cluster.adapt(minimum_jobs=0, maximum_jobs=10)
+    # client = Client(cluster)
+    # print(cluster.job_script())
     backend = "dask" if not args.no_dask else "loky"
     par_args = (
         {"wait_for_workers_timeout": 0} if not args.no_dask else {"n_jobs": args.cores}
     )
     with joblib.parallel_backend(backend, **par_args):
-        main(adata)
+        for fname, lst in FEATURE_LISTS.items():
+            if lst is None:
+                main(adata, fname)
+            else:
+                mask = adata.var["GENEID"].isin(lst)
+                main(adata[:, mask], fname)
