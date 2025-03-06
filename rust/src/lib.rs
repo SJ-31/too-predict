84da208cbd8429372ee34832aac9eabae2f7f260
Shann Chongwattanankul <shanj3131@gmail.com>
2025-03-06 15:00:54 +0700

    feat: add function for encoding expression relationships

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,17 +1,22 @@
 use core::f64;
 
-use ndarray::{s, Array1, Array2, ArrayBase, ArrayView1, ArrayView2, Axis};
+use itertools::Itertools;
+use ndarray::{s, Array1, Array2, ArrayBase, ArrayView1, ArrayView2, Axis, Zip};
 use ndarray_stats::CorrelationExt;
+use num_traits::zero;
 use numpy::{IntoPyArray, PyArray2, PyReadonlyArray2};
 use pyo3::types::PyModuleMethods;
 use pyo3::wrap_pyfunction;
 use pyo3::{pyfunction, pymodule, types::PyModule, Bound, PyResult, Python};
 use rayon::prelude::*;
 
+// * Send to python
+
 #[pymodule]
 fn _rust_helpers(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(phi_matrix, m)?)?;
     m.add_function(wrap_pyfunction!(rho_matrix, m)?)?;
+    m.add_function(wrap_pyfunction!(encode_pairs, m)?)?;
     Ok(())
 }
 
@@ -27,6 +32,16 @@ fn phi_matrix<'py>(
 }
 
 #[pyfunction]
+fn encode_pairs<'py>(
+    py: Python<'py>,
+    arr: PyReadonlyArray2<'py, f64>,
+) -> Bound<'py, PyArray2<i64>> {
+    let converted: ArrayView2<f64> = arr.as_array();
+    let result = encode_pairs_rs(converted);
+    result.into_pyarray_bound(py)
+}
+
+#[pyfunction]
 fn rho_matrix<'py>(
     py: Python<'py>,
     arr: PyReadonlyArray2<'py, f64>,
@@ -36,6 +51,8 @@ fn rho_matrix<'py>(
     let result = proportionality_coeff_rs(converted, do_parallel);
     result.into_pyarray_bound(py)
 }
+
+// * Rust implementations
 
 fn phi_proportionality_test(x: ArrayView1<f64>, y: ArrayView1<f64>) -> f64 {
     let log = x.ln();
@@ -118,6 +135,39 @@ fn phi_proportionality_rs(arr: ArrayView2<f64>, do_parallel: bool) -> Array2<f64
     result
 }
 
+fn factorial(n: u64) -> u64 {
+    (1..=n).product()
+}
+
+fn choose(n: u64, r: u64) -> u64 {
+    factorial(n) / (factorial(r) * factorial(n - r))
+}
+
+/// Encode gene expression pairs as binary indicators, adapted from RaMBat [1]
+///
+/// # Arguments
+/// * arr : An array of shape [ n_samples, n_features ]
+///
+/// # Returns
+/// A binary array of shape [ n_samples, choose(n_features, 2) ]
+///
+fn encode_pairs_rs(arr: ArrayView2<f64>) -> Array2<i64> {
+    let ncols = arr.nrows();
+    let n_features = arr.ncols();
+    let mut result: Array2<i64> = Array2::zeros([ncols, choose(n_features as u64, 2) as usize]);
+    for (i, pair) in (0..n_features).combinations(2).enumerate() {
+        let f1: ArrayView1<f64> = arr.slice(s![.., pair[0]]);
+        let f2: ArrayView1<f64> = arr.slice(s![.., pair[1]]);
+        let comparison = Zip::from(f1)
+            .and(f2)
+            .map_collect(|x, y| if x > y { 1 } else { 0 });
+        result.slice_mut(s![.., i]).assign(&comparison);
+    }
+    result
+}
+
+// * Tests
+
 #[test]
 fn test_phi_prop() {
     use ndarray::arr2;
@@ -125,14 +175,29 @@ fn test_phi_prop() {
     let dim = h.raw_dim();
     let s1 = h.slice(s![0, ..]);
     let s2 = h.slice(s![1, ..]);
+    let comparison = Zip::from(s1)
+        .and_broadcast(s2)
+        .map_collect(|x, y| if x > y { 1 } else { 0 });
     let phi2 = phi_proportionality_rs(h.view(), false);
-    println!("{:?}", phi2);
+    let encode2 = encode_pairs_rs(h.view());
+    println!("foo");
+    println!("{:?}", encode2);
 }
 
+#[ignore]
 #[test]
 fn test_prop_coeff() {
     use ndarray::arr2;
     let h = arr2(&[[8, 1, 2, 3], [4, 5, 6, 7]]).mapv(|x| x as f64);
     let rho = proportionality_coeff_rs(h.view(), false);
     println!("{:?}", rho);
+}
+
+#[ignore]
+#[test]
+fn test_combos() {
+    let myvec = Vec::from_iter(0..10);
+    for (i, b) in myvec.into_iter().combinations(2).enumerate() {
+        println!("{:?}, {:?}", i, b[0]);
+    }
 }
