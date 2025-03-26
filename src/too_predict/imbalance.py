d9a894d3afafa5e35438624739f0569e6e2db28a
Shann Chongwattanankul <shanj3131@gmail.com>
2025-03-26 13:20:24 +0700

    chore: add helper class for custom resampling

--- src/too_predict/imbalance.py
+++ src/too_predict/imbalance.py
@@ -3,6 +3,7 @@ import anndata as ad
 import imblearn.combine as icc
 import imblearn.over_sampling as ios
 import imblearn.under_sampling as ius
+import numpy as np
 import pandas as pd
 from scanpy import AnnData
 
@@ -85,3 +86,64 @@ class Balancer:
                 X=resampled_x, var=self.adata.var, obs=pd.DataFrame({self.label_col: y})
             )
         return new
+
+
+def spaced_resample(
+    labels: np.ndarray | pd.Series,
+    targets: dict[str, int | None] | None = None,
+    undersample: bool = False,
+    bin_step: int = 1,
+    n_bins: int = 10,
+    space: str = "geom",
+):
+    """Determine counts for each label by incrementing along a sequence
+
+      This function creates a regularly-spaced sequence from the counts of the
+    given labels with a specified numpy function (geom, histogram or linspace).
+    The new value for each label is obtained by first indexing it on the sequence,
+    then adding/subtracting the offset `bin_step` in an undersampling or oversampling
+    context respectively
+
+    Parameters
+    ----------
+    labels : array of class labels i.e. `y` passed to model.fit(X, y)
+    targets : dictionary of label->bin_step or None to use the default bin step
+        if the value is a float, it is interpreted as a scaling factor
+    bin_step : the offset of the bins with which to promote/demote the current label
+        to
+    n_bins : the length of the space. The higher the less aggressive the sampling
+
+    Returns
+    -------
+    dictionary mapping labels to their new counts
+    """
+    counts: pd.Series = (
+        labels.value_counts()
+        if isinstance(labels, pd.Series)
+        else pd.Series(labels).value_counts()
+    )
+    count_dict: dict = counts.to_dict()
+    match space:
+        case "geom":
+            vals = np.geomspace(counts.min(), counts.max(), num=n_bins)
+        case "linspace":
+            vals = np.linspace(counts.min(), counts.max(), num=n_bins)
+        case "hist" | "histogram":
+            _, vals = np.histogram(counts, bins=n_bins)
+        case _:
+            raise ValueError(f"Space {space} not supported!")
+    for label, c in count_dict.items():
+        if targets is not None and label not in targets:
+            continue
+        elif targets is not None and (s := targets.get(label)):
+            if isinstance(s, float):
+                count_dict[label] = round(c * s)
+                continue
+            step = s
+        else:  # `targets` not given
+            step = bin_step
+        locs = np.where(vals <= c)[0]
+        new_loc: int = locs.max() + step if not undersample else locs.max() - step
+        new_loc = min(new_loc, len(vals) - 1) if not undersample else max(new_loc, 0)
+        count_dict[label] = round(vals[new_loc])
+    return count_dict
