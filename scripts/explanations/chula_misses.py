9dd8fcfb378653df5511be7feb5e6511f741daaf
Shann Chongwattanankul <shanj3131@gmail.com>
2025-03-28 09:23:59 +0700

    chore: try use explain class to improve acc

--- scripts/explanations/chula_misses.py
+++ scripts/explanations/chula_misses.py
@@ -6,10 +6,11 @@ import anndata as ad
 import joblib
 import pandas as pd
 import shap
-import too_predict.evaluation as te
+import too_predict.explanation as ex
 from dask.distributed import Client
 from dask_jobqueue import SLURMCluster
 from pyhere import here
+from too_predict.evaluation import get_all_metrics, write_cross_val
 from too_predict.filter import Filter
 from too_predict.imputer import Imputer
 from too_predict.model import PredBase, XGBEstimator
@@ -54,6 +55,8 @@ def parse_args():
     parser.add_argument("-c", "--cores", default=8)
     parser.add_argument("-t", "--test", default=False, action="store_true")
     parser.add_argument("-d", "--dask", default=False, action="store_true")
+    parser.add_argument("--no_shap", default=False, action="store_true")
+    parser.add_argument("--no_morris", default=False, action="store_true")
     parser.add_argument("-l", "--label_class", default="tumor_type")
     return parser.parse_args()
 
@@ -64,43 +67,86 @@ def shap_helper(
     test: ad.AnnData,
     label_col: str,
     set_name: str,
-) -> pd.DataFrame:
-    model.fit(train)
+):
+    outdir = OUTDIR.joinpath("shapley")
     explainer = shap.TreeExplainer(model.get_model())
-    s_test = te.get_shapley_adata(
-        test, explainer, model, label_col, feature_col="GENEID"
+    s_test, s_vals = ex.get_shap_adata(
+        test,
+        explainer,
+        model,
+        label_col,
+        feature_col="GENEID",
+        summary_plot=True,
+        plot_feature_col="GENENAME",
+        plot_directory=here(outdir.joinpath(f"{set_name}_plots")),
     )
-    s_test.loc[:, "dataset"] = set_name
-    return s_test
+    s_test.obs.loc[:, "dataset"] = set_name
+    # write_pickle(s_vals, here(outdir.joinpath(f"shapley_explanation-{set_name}.pkl")))
+    s_train, _ = ex.get_shap_adata(
+        train,
+        explainer,
+        model,
+        label_col,
+        feature_col="GENEID",
+        summary_plot=False,
+        plot_directory=None,
+    )
+    s_test.write_h5ad(here(outdir.joinpath(f"shapley-{set_name}.h5ad")))
+    ff = ex.Explain(s_train, s_test, label_col=label_col)
+    neg_contrib, per_label = ff.shap_neg_contributions()
+    return neg_contrib
 
 
-def main(test: bool = True, label_col: str = "tumor_type"):
-    if test:
+def main(args):
+    if args.test:
         adata = training_data_internal_test()
     else:
         adata = training_data_internal()
 
-    filter = Filter(
-        feature_col="GENEID", features=FEATURES["edgeR_median_lfc_feature_list_3000"]
-    )
+    feature_set = "edgeR_median_lfc_feature_list_3000"
+    filter = Filter(feature_col="GENEID", features=FEATURES[feature_set])
     t = Transformer("clr", impute_fn=Imputer("plus_one"), inplace=False)
 
     adata = filter.fit_transform(adata)
     adata = t.fit_transform(adata)
     adata.X = adata.X.toarray()
 
-    model = PredBase(model=XGBEstimator())
-    all_shap_tmp = []
-    for name, fn in ADDITIONAL_SPLITS.items():
-        _, test = fn(adata)
-        unique_values = test.obs[label_col].unique()
-        spec = {label_col: [(u, 5) for u in unique_values]}
-        train, test = split_and_sample(adata, fn, spec, RNG)
-        all_shap_tmp.append(
-            shap_helper(model, train, test, label_col=label_col, set_name=name)
-        )
-    all_shap = pd.concat(all_shap_tmp)
-    all_shap.to_csv(OUTDIR.joinpath("shapley_results.csv"), index=False)
+    label_col = args.label_class
+    if not args.no_shap:
+        model = PredBase(model=XGBEstimator())
+        for name, fn in ADDITIONAL_SPLITS.items():
+            _, test = fn(adata)
+            unique_values = test.obs[label_col].unique()
+            spec = {label_col: [(u, 5) for u in unique_values]}
+            train, test = split_and_sample(adata, fn, spec, RNG)
+            model.fit(train)
+            neg_contrib = shap_helper(
+                model, train, test, label_col=label_col, set_name=name
+            )
+            new_filter = Filter(
+                feature_col="GENEID",
+                features=FEATURES[feature_set],
+                blacklist=neg_contrib,
+            )
+            proba = model.predict_proba(test)
+            perf = get_all_metrics(test.obs[label_col], proba, model.classes_)
+            metrics = (
+                f"acc: {perf.get('acc')}\nbalanced_acc: {perf.get('balanced_acc')}\n"
+                f"kappa: {perf.get('kappa')}"
+            )
+            OUTDIR.joinpath("shapley").joinpath(f"{name}_before.txt").write_text(
+                metrics
+            )
+
+            no_neg_contrib = new_filter.fit_transform(adata)
+            # See how the model performs after removing the negatively contributing
+            # features
+            updated_perf = model.holdout(  # Will refit in here
+                no_neg_contrib,
+                {k: v for k, v in ADDITIONAL_SPLITS if k == name},
+                label_col=label_col,
+            )
+            write_cross_val(updated_perf, OUTDIR.joinpath("shapley"), prefix=name)
 
 
 if __name__ == "__main__":
@@ -111,4 +157,4 @@ if __name__ == "__main__":
     backend = "dask" if args.dask else "loky"
     par_args = {"wait_for_workers_timeout": 0} if args.dask else {"n_jobs": args.cores}
     with joblib.parallel_backend(backend, **par_args):
-        main(args.test, args.label_class)
+        main(args)
