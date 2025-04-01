9b48fcc8ec7103cd2da59b0e038694dc8c2b4fa5
Shann Chongwattanankul <shanj3131@gmail.com>
2025-04-01 17:28:24 +0700

    chore: refactor cross-validation data

--- scripts/cross_validate.py
+++ scripts/cross_validate.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env ipython
+from datetime import datetime
 from pathlib import Path
 
 import anndata as ad
 import joblib
 import pandas as pd
-import too_predict.model as tm
 from dask.distributed import Client
 from dask_jobqueue import SLURMCluster
 from pyhere import here
 from scanpy import read_h5ad
-from sklearn.ensemble import HistGradientBoostingClassifier, RandomForestClassifier
+from too_predict._train_utils import (
+    ADDITIONAL_SPLITS,
+    FEATURE_LISTS,
+    MODELS,
+    REF_LISTS,
+    read_model_spec,
+)
 from too_predict.filter import Filter
-from too_predict.imbalance import Balancer
-from too_predict.imputer import IMPLEMENTED_IMPUTATION, Imputer
+from too_predict.imputer import Imputer
 from too_predict.transformer import Transformer
 from too_predict.utils import (
     RANDOM_STATE,
-    RNG,
     recode_to_go,
-    ref_feature_lists_internal,
     training_data_internal,
     training_data_internal_test,
+    write_pickle,
 )
 
 OUTDIR: Path = here("data", "output", "cross_validation")
@@ -42,220 +46,75 @@ def parse_args():
     return parser.parse_args()
 
 
+def get_debug_file():
+    file = datetime.now().strftime("%y-%m-%d_%H:%M")
+    file = here("remote", "repos", "too-predict", f"cross_val_debug_{file}.pkl")
+    return file
+
+
 STORAGE_DIR = here("remote", "repos", "too-predict", "normalization_comparison")
 LABEL_CLASSES = ["tumor_type", "primary_site"]
 GROUP_CLASSES = ["Project_ID"]
-REF_LISTS, FEATURE_LISTS = ref_feature_lists_internal()
 USE_CACHED: bool = True
 
 # Dictionary of model_name -> [model, transformation, imputation, feature_set]
 # TODO: should you test on every available combination?
-MODELS: dict = {
-    "clr_random_forest_minfo": {
-        "model": tm.RandomForestPred(),
-        "t": "clr",
-        "i": "plus_one",
-        "f": "mutual_info_feature_list_3000",
-    },
-    "clr_rf_edger_low_variance_ref": {
-        "model": tm.RandomForestPred(),
-        "t": "clr",
-        "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_3000",
-        "r": "variance_feature_list_lowest_20",
-    },
-    "clr_xgboost_edger_GO": {
-        "model": tm.PredBase(model=tm.XGBEstimator()),
-        "t": "clr",
-        "i": "plus_one",
-        "f": "edgeR_go_feature_list_40",
-    },
-    "clr_xgboost_variance_GO": {
-        "model": tm.PredBase(model=tm.XGBEstimator()),
-        "t": "clr",
-        "i": "plus_one",
-        "f": "variance_go_feature_list_1500",
-    },
-    "clr_xgboost_edger_smote": {
-        "model": tm.PredBase(model=tm.XGBEstimator()),
-        "t": "clr",
-        "i": "plus_one",
-        "f": "mutual_info_feature_list_3000",
-        "b": Balancer("SMOTE"),
-    },
-    "qsmooth_xgboost_edger_1000": {
-        "model": tm.PredBase(model=tm.XGBEstimator()),
-        "t": "qsmooth",
-        "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_1000",
-    },
-    "clr_xgboost_edger": {
-        "model": tm.PredBase(model=HistGradientBoostingClassifier(), make_dense=True),
-        "t": "clr",
-        "i": "plus_one",
-        "f": "mutual_info_feature_list_3000",
-    },
-    "clr_random_forest_edger": {
-        "model": tm.RandomForestPred(),
-        "t": "clr",
-        "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_3000",
-    },
-    "alr_xgboost_low_variance": {
-        "model": tm.AlrBase(
-            tm.XGBEstimator(),
-            references=REF_LISTS["variance_feature_list_lowest_20"],
-        ),
-        "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_3000",
-        "r": "variance_feature_list_lowest_20",
-    },
-    "alr_xgboost_low_variance_1000": {
-        "model": tm.AlrBase(
-            tm.XGBEstimator(),
-            references=REF_LISTS["variance_feature_list_lowest_20"],
-        ),
-        "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_1000",
-        "r": "variance_feature_list_lowest_20",
-    },
-    "alr_random_forest_low_variance": {
-        "model": tm.AlrBase(
-            RandomForestClassifier(random_state=RNG),
-            references=REF_LISTS["variance_feature_list_lowest_20"],
-        ),
-        "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_3000",
-        "r": "variance_feature_list_lowest_20",
-    },
-    "alr_random_forest_edger_lfc": {
-        "model": tm.AlrBase(
-            RandomForestClassifier(random_state=RNG),
-            references=REF_LISTS["edgeR_median_lfc_feature_list_lowest_20"],
-        ),
-        "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_1000",
-        "r": "edgeR_median_lfc_feature_list_lowest_20",
-    },
-    "tmm_random_forest_edger": {
-        "model": tm.RandomForestPred(),
-        "t": "tmm",
-        "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_3000",
-    },
-    "tpm_random_forest_edger": {
-        "model": tm.RandomForestPred(),
-        "t": "tpm",
-        "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_3000",
-    },
-    "fpkm_random_forest_edger": {
-        "model": tm.RandomForestPred(),
-        "t": "fpkm",
-        "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_3000",
-    },
-    "dirichlet_random_forest_edger": {
-        "model": tm.SimPred(
-            RandomForestClassifier(random_state=RNG), method="dirichlet"
-        ),
-        "t": "none",
-        "i": "none",
-        "f": "edgeR_median_lfc_feature_list_3000",
-    },
-}
 
 ADATA: ad.AnnData
-
-# [2025-03-11 Tue]
-# Want to see how well models handle Chula organoids and datasets from other projects
-# Ideally this should be done with StratifiedGroupKFold but
-# grouping is problematic because some groups are confounded
-# with whatever you are labeling on
-# This means that some instances won't be seen at all in the test data
-# gc is the group variable excluded during the cv folds e.g. Sample_Type
-ADDITIONAL_SPLITS: dict = {
-    "CHULA": lambda x: (
-        x[~x.obs["Project_ID"].str.contains("CHULA"), :],
-        x[x.obs["Project_ID"].str.contains("CHULA"), :],
-    ),
-    "CGCI": lambda x: (
-        x[~x.obs["Project_ID"].str.contains("CGCI"), :],
-        x[x.obs["Project_ID"].str.contains("CGCI"), :],
-    ),
-    "CPTAC": lambda x: (
-        x[~x.obs["Project_ID"].str.contains("CPTAC"), :],
-        x[x.obs["Project_ID"].str.contains("CPTAC"), :],
-    ),
-    "GEO": lambda x: (
-        x[~x.obs["Project_ID"].str.contains("GSE"), :],
-        x[x.obs["Project_ID"].str.contains("GSE"), :],
-    ),
-}
 
 
 def cross_validate_helper(
     lc,
     gc,
-    model,
+    spec: dict,
     result_dir_str,
     trans,
     impute,
     feature_set,
-    references=None,
-    balancer=None,
+    skip: bool = False,
 ):
+    if skip:
+        return
     adata = ADATA.copy()
-    if gc is not None:
-        result_dir: Path = here(OUTDIR, f"{result_dir_str}_by_group_{gc}")
-    else:
-        result_dir: Path = here(OUTDIR, result_dir_str)
-    result_dir.mkdir(exist_ok=True, parents=True)
-    dir = STORAGE_DIR.joinpath(feature_set)
-    output = here(dir, f"{trans}-{impute}.h5ad")
-    transformer = None
-    if (not output.exists() or trans is None) or not USE_CACHED:
-        if "GO" in result_dir_str:
-            adata = recode_to_go(adata)
-            print(adata)
-        if feat := FEATURE_LISTS[feature_set]:
-            if "GO" not in result_dir_str:
-                F: Filter = Filter(
-                    feat if references is None else feat + REF_LISTS[references],
-                    feature_col="GENEID",
-                )
-            else:
-                F = Filter(feat, feature_col="GO accession")
-            adata = F.fit_transform(adata)
-        kwargs = {}
-        if trans == "clr" and references is not None:
-            kwargs = {"features": REF_LISTS[references], "feature_col": "GENEID"}
-        transformer = Transformer(
-            trans, impute_fn=Imputer(impute), inplace=False, **kwargs
-        )
-        # Does nothing if trans is None
-    else:
-        adata = read_h5ad(output)
-    if not here(result_dir, f"{lc}-misc.csv").exists() and DO_CV:
-        track_meta = result_dir.joinpath(".metadata")
-        track_meta.mkdir(exist_ok=True)
-        results = model.cross_validate(
-            adata,
-            label_col=lc,
-            group_col=gc,
-            random_state=RANDOM_STATE,
-            n_splits=K,
-            transformer=transformer,
-            record_dir=track_meta,
-        )
-        write_results(results, result_dir, lc, cm_prefix="fold_")
-    result_dir2 = result_dir.joinpath("additional_splits")
-    result_dir2.mkdir(exist_ok=True, parents=True)
-    if not here(result_dir2, f"{lc}-misc.csv").exists():
-        results2 = model.holdout(
-            adata, ADDITIONAL_SPLITS, label_col=lc, balancer=balancer
-        )
-        write_results(results2, result_dir2, lc)
+    filter, model, transformer, balancer, encoder = read_model_spec(spec)
+    try:
+        if gc is not None:
+            result_dir: Path = here(OUTDIR, f"{result_dir_str}_by_group_{gc}")
+        else:
+            result_dir: Path = here(OUTDIR, result_dir_str)
+        result_dir.mkdir(exist_ok=True, parents=True)
+        dir = STORAGE_DIR.joinpath(feature_set)
+        output = here(dir, f"{trans}-{impute}.h5ad")
+
+        if USE_CACHED:
+            adata = read_h5ad(output)
+        else:
+            adata = encoder(adata)
+            adata = filter.fit_transform(adata)
+        if not here(result_dir, f"{lc}-misc.csv").exists() and DO_CV:
+            track_meta = result_dir.joinpath(".metadata")
+            track_meta.mkdir(exist_ok=True)
+            results = model.cross_validate(
+                adata,
+                label_col=lc,
+                group_col=gc,
+                random_state=RANDOM_STATE,
+                n_splits=K,
+                transformer=transformer,
+                record_dir=track_meta,
+            )
+            write_results(results, result_dir, lc, cm_prefix="fold_")
+        result_dir2 = result_dir.joinpath("additional_splits")
+        result_dir2.mkdir(exist_ok=True, parents=True)
+        if not here(result_dir2, f"{lc}-misc.csv").exists():
+            results2 = model.holdout(
+                adata, ADDITIONAL_SPLITS, label_col=lc, balancer=balancer
+            )
+            write_results(results2, result_dir2, lc)
+    except ValueError:
+        print(f"ValueError encountered with params {spec}")
+        print("Writing pckl...")
+        write_pickle(data, get_debug_file())
 
 
 def write_results(results, result_dir, label_col, cm_prefix: str = ""):
@@ -291,22 +150,13 @@ if __name__ == "__main__":
 
     with joblib.parallel_backend(backend):
         for name, data in MODELS.items():
-            model, transformation, imputation, features, references, balancer = (
-                data.get("model"),
-                data.get("t"),
-                data.get("i"),
-                data.get("f"),
-                data.get("r"),
-                data.get("b"),
-            )
+            t, i, skip = data.get("t"), data.get("i"), data.get("s")
             cross_validate_helper(
                 lc=label_class,
                 gc=None,
-                model=model,
+                spec=data,
                 result_dir_str=name,
-                trans=transformation,
-                feature_set=features,
-                impute=imputation,
-                references=references,
-                balancer=balancer,
+                trans=t,
+                impute=i,
+                skip=skip,
             )
---
+++ src/too_predict/_train_utils.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env ipython
+from typing import Callable
+
+from interpret.glassbox import ExplainableBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+
+import too_predict.model as tm
+from too_predict.filter import Filter
+from too_predict.imbalance import Balancer
+from too_predict.imputer import Imputer
+from too_predict.model import PredBase, RandomForestClassifier
+from too_predict.transformer import Transformer
+from too_predict.utils import RNG, recode_to_go, ref_feature_lists_internal
+
+REF_LISTS, FEATURE_LISTS = ref_feature_lists_internal()
+
+# * Models
+# Key:
+# m : model
+# i : imputation
+# t : transformation
+# f : feature set
+# r : reference set
+# e : encoding (only "GO" and None) are supported [2025-04-01 Tue]
+# k : kwargs to transformer
+
+MODELS: dict = {
+    "clr_random_forest_minfo": {
+        "m": tm.RandomForestPred(),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "mutual_info_feature_list_3000",
+        "s": True,
+    },
+    "clr_xgboost_edger_low_variance_ref": {
+        "m": tm.PredBase(model=tm.XGBEstimator()),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+        "r": "variance_feature_list_lowest_20",
+    },
+    "clr_xgboost_edger_GO": {
+        "m": tm.PredBase(model=tm.XGBEstimator()),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "edgeR_go_feature_list_40",
+        "e": "GO",
+        "s": True,
+    },
+    "clr_xgboost_variance_GO": {
+        "m": tm.PredBase(model=tm.XGBEstimator()),
+        "t": "clr",
+        "i": "plus_one",
+        "e": "GO",
+        "f": "variance_go_feature_list_1500",
+        "s": True,
+    },
+    "clr_xgboost_edger_smote": {
+        "m": tm.PredBase(model=tm.XGBEstimator()),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+        "b": Balancer("SMOTE"),
+        "s": True,
+    },
+    "qsmooth_xgboost_edger_1000": {
+        "m": tm.PredBase(model=tm.XGBEstimator()),
+        "t": "qsmooth",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_1000",
+        "s": True,
+    },
+    "clr_lr_edger_3000": {
+        "m": tm.PredBase(model=LogisticRegression(solver="saga")),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+    },
+    "clr_xgboost_edger_1000_undersample": {
+        "m": tm.PredBase(model=tm.XGBEstimator()),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_1000",
+        "b": Balancer(method="RandomUnderSampler", sampling_strategy="not minority"),
+    },
+    "clr_xgb3_edger": {
+        "m": tm.PredBase(model=tm.XGBEstimator(max_depth=3)),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+    },
+    "clr_xgboost_edger": {
+        "m": tm.PredBase(model=tm.XGBEstimator(), make_dense=True),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+    },
+    "batch_xgboost_rf_edger": {  # [2025-03-27 Thu] TODO: choose a fast m for `outer`
+        "m": tm.BatchBase(inner=tm.XGBEstimator(), outer_y="", outer=None),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_1000",
+    },
+    "clr_ebm_edger": {
+        "m": tm.PredBase(model=ExplainableBoostingClassifier(n_jobs=-2)),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+        "s": True,  # [2025-03-27 Thu] Want to try this out badly, but it's so slow
+    },
+    # "clr_dt_edger": {  # A surrogate model
+    #     "model": tm.PredBase(model=DecisionTreeClassifier()),
+    #     "t": "clr",
+    #     "i": "plus_one",
+    #     "f": "edgeR_median_lfc_feature_list_1000",
+    # },
+    "clr_random_forest_edger": {
+        "m": tm.RandomForestPred(),
+        "t": "clr",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+        "s": True,
+    },
+    "alr_xgboost_low_variance": {
+        "m": tm.AlrBase(
+            tm.XGBEstimator(),
+            references=REF_LISTS["variance_feature_list_lowest_20"],
+        ),
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+        "r": "variance_feature_list_lowest_20",
+    },
+    "alr_xgboost_low_variance_1000": {
+        "m": tm.AlrBase(
+            tm.XGBEstimator(),
+            references=REF_LISTS["variance_feature_list_lowest_20"],
+        ),
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_1000",
+        "r": "variance_feature_list_lowest_20",
+    },
+    "alr_random_forest_low_variance": {
+        "m": tm.AlrBase(
+            RandomForestClassifier(random_state=RNG),
+            references=REF_LISTS["variance_feature_list_lowest_20"],
+        ),
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+        "r": "variance_feature_list_lowest_20",
+        "s": True,
+    },
+    "alr_random_forest_edger_lfc": {
+        "m": tm.AlrBase(
+            RandomForestClassifier(random_state=RNG),
+            references=REF_LISTS["edgeR_median_lfc_feature_list_lowest_20"],
+        ),
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_1000",
+        "r": "edgeR_median_lfc_feature_list_lowest_20",
+        "s": True,
+    },
+    "tmm_random_forest_edger": {
+        "m": tm.RandomForestPred(),
+        "t": "tmm",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+        "s": True,
+    },
+    "tpm_random_forest_edger": {
+        "m": tm.RandomForestPred(),
+        "t": "tpm",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+        "s": True,
+    },
+    "fpkm_random_forest_edger": {
+        "m": tm.RandomForestPred(),
+        "t": "fpkm",
+        "i": "plus_one",
+        "f": "edgeR_median_lfc_feature_list_3000",
+        "s": True,
+    },
+    "dirichlet_random_forest_edger": {
+        "m": tm.SimPred(RandomForestClassifier(random_state=RNG), method="dirichlet"),
+        "t": "none",
+        "i": "none",
+        "f": "edgeR_median_lfc_feature_list_3000",
+        "s": True,
+    },
+}
+
+# * Additional splits
+# [2025-03-11 Tue]
+# Want to see how well models handle Chula organoids and datasets from other projects
+# Ideally this should be done with StratifiedGroupKFold but
+# grouping is problematic because some groups are confounded
+# with whatever you are labeling on
+# This means that some instances won't be seen at all in the test data
+# gc is the group variable excluded during the cv folds e.g. Sample_Type
+ADDITIONAL_SPLITS: dict = {
+    "CHULA": lambda x: (
+        x[~x.obs["Project_ID"].str.contains("CHULA"), :],
+        x[x.obs["Project_ID"].str.contains("CHULA"), :],
+    ),
+    "CGCI": lambda x: (
+        x[~x.obs["Project_ID"].str.contains("CGCI"), :],
+        x[x.obs["Project_ID"].str.contains("CGCI"), :],
+    ),
+    "CPTAC": lambda x: (
+        x[~x.obs["Project_ID"].str.contains("CPTAC"), :],
+        x[x.obs["Project_ID"].str.contains("CPTAC"), :],
+    ),
+    "GEO": lambda x: (
+        x[~x.obs["Project_ID"].str.contains("GSE"), :],
+        x[x.obs["Project_ID"].str.contains("GSE"), :],
+    ),
+}
+
+
+# * Helper functions
+def read_model_spec(
+    spec: dict,
+) -> tuple[Filter, PredBase, Transformer, Balancer | None, Callable]:
+    M: PredBase = spec.get("model")
+    references = spec.get("r")
+    features = spec.get("f")
+    encoding = spec.get("e")
+
+    if encoding == "GO":
+        encode_fn = lambda x: recode_to_go(x)
+        fcol = "GO accession"
+    else:
+        encode_fn = lambda x: x
+        fcol = "GENEID"
+    F = Filter(
+        features if references is None else features + REF_LISTS[references],
+        feature_col=fcol,
+    )
+    B: Balancer = spec.get("b")
+    transformation_name = spec.get("t")
+    kwargs: dict = spec.get("k", {})
+    if transformation_name == "clr" and references is not None:
+        kwargs.update({"features": REF_LISTS[references], "feature_col": "GENEID"})
+    T = Transformer(
+        transformation_name, impute_fn=Imputer(spec.get("i")), inplace=False, **kwargs
+    )
+    return F, M, T, B, encode_fn
