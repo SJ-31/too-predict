f9bbf6010405da031b025ba27952267c11df8e65
Shann Chongwattanankul <shanj3131@gmail.com>
2025-04-02 10:50:46 +0700

    feat: finalize anchor and shapley script

--- scripts/explanations/chula_misses.py
+++ scripts/explanations/chula_misses.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env ipython
 
 from pathlib import Path
+from typing import Callable
 
 import anndata as ad
 import joblib
@@ -11,45 +12,21 @@ import too_predict.explanation as ex
 from dask.distributed import Client
 from dask_jobqueue import SLURMCluster
 from pyhere import here
+from too_predict._train_utils import ADDITIONAL_SPLITS, MODELS, read_model_spec
 from too_predict.evaluation import get_all_metrics, write_cross_val, write_metrics
 from too_predict.filter import Filter
-from too_predict.imputer import Imputer
-from too_predict.model import PredBase, XGBEstimator
+from too_predict.model import PredBase
 from too_predict.plotting import plot_diagonal_matrix
-from too_predict.transformer import Transformer
 from too_predict.utils import (
     RNG,
-    load_pickle,
-    ref_feature_lists_internal,
     split_and_sample,
     training_data_internal,
     training_data_internal_test,
-    write_pickle,
 )
 
 OUTDIR: Path = here("data", "output", "explanations", "chula_misses")
 OUTDIR.mkdir(exist_ok=True, parents=True)
 STORAGE: Path = here("remote", "repos", "too-predict")
-REFS, FEATURES = ref_feature_lists_internal()
-
-ADDITIONAL_SPLITS: dict = {
-    "CHULA": lambda x: (
-        x[~x.obs["Project_ID"].str.contains("CHULA"), :],
-        x[x.obs["Project_ID"].str.contains("CHULA"), :],
-    ),
-    "CGCI": lambda x: (
-        x[~x.obs["Project_ID"].str.contains("CGCI"), :],
-        x[x.obs["Project_ID"].str.contains("CGCI"), :],
-    ),
-    "CPTAC": lambda x: (
-        x[~x.obs["Project_ID"].str.contains("CPTAC"), :],
-        x[x.obs["Project_ID"].str.contains("CPTAC"), :],
-    ),
-    "GEO": lambda x: (
-        x[~x.obs["Project_ID"].str.contains("GSE"), :],
-        x[x.obs["Project_ID"].str.contains("GSE"), :],
-    ),
-}
 
 
 def parse_args():
@@ -59,11 +36,41 @@ def parse_args():
     parser.add_argument("-m", "--memory", default="30")
     parser.add_argument("-c", "--cores", default=8)
     parser.add_argument("-t", "--test", default=False, action="store_true")
+    parser.add_argument("-i", "--ignored", default="")
     parser.add_argument("-d", "--dask", default=False, action="store_true")
     parser.add_argument("--no_shap", default=False, action="store_true")
     parser.add_argument("--no_morris", default=False, action="store_true")
     parser.add_argument("-l", "--label_class", default="tumor_type")
     return parser.parse_args()
+
+
+def anchor_helper(
+    model: PredBase,
+    train: ad.AnnData,
+    test: ad.AnnData,
+    label_col: str,
+    set_name: str,
+    n: int = 10,
+):
+    # [2025-04-01 Tue] Use anchors to identify which features are responsible
+    # for causing consistent misclassifications
+    outdir = OUTDIR.joinpath("anchor")
+    exp = ex.Exp(model, adata=test, feature_col="GENEID", label_col=label_col)
+    s_test, test_metrics = exp.anchor()
+    outfile = here(outdir.joinpath(f"anchor-{set_name}.h5ad"))
+    s_train, train_metrics = exp.anchor(train)
+
+    test_metrics.loc[:, "set"] = "test"
+    train_metrics.loc[:, "set"] = "train"
+    all_metrics = pd.concat([test_metrics, train_metrics])
+    all_metrics.to_csv(here(outdir.joinpath(f"anchor-{set_name}.csv")), index=False)
+
+    if not outfile.exists():
+        s_test.write_h5ad(here(outdir.joinpath(f"anchor-{set_name}.h5ad")))
+
+    ff = ex.ExpInterpreter(s_train, s_test, label_col=label_col)
+    neg_contrib, per_label = ff.neg_contributions(prefix="anchor_", n=n)
+    return neg_contrib
 
 
 def shap_helper(
@@ -95,7 +102,7 @@ def shap_helper(
     if not outfile.exists():
         s_test.write_h5ad(here(outdir.joinpath(f"shapley-{set_name}.h5ad")))
     ff = ex.ExpInterpreter(s_train, s_test, label_col=label_col)
-    neg_contrib, per_label = ff.shap_neg_contributions(n=n)
+    neg_contrib, per_label = ff.neg_contributions("shap_", n=n)
     train_mat = ff.shap_distance(target="train", square=True)
     fig, ax = plt.subplots()
     plot_diagonal_matrix(train_mat, ax, cmap="coolwarm")
@@ -109,71 +116,70 @@ def shap_helper(
     return neg_contrib
 
 
+def remove_zero_features(
+    adata, filter: Filter, model: PredBase, splitter: Callable
+) -> tuple[ad.AnnData, ad.AnnData]:
+    train, _ = splitter(adata)
+    model.fit(train)
+    new_filter = filter.copy()
+    new_filter.from_feature_importance(model)
+    zeros_removed = new_filter.fit_transform(adata)
+    train2, test2 = splitter(zeros_removed)
+    model.fit(train2)
+    return train2, test2
+
+
 def main(args):
     if args.test:
         adata = training_data_internal_test()
     else:
         adata = training_data_internal()
+    ignored = args.ignore.split(",")
 
-    feature_set = "edgeR_median_lfc_feature_list_3000"
-    filter = Filter(feature_col="GENEID", features=FEATURES[feature_set])
-    t = Transformer("clr", impute_fn=Imputer("plus_one"), inplace=False)
+    spec = MODELS["clr_xgboost_edger"]
+    F, M, T, B, E = read_model_spec(spec)
 
-    adata = filter.fit_transform(adata)
-    adata = t.fit_transform(adata)
+    adata = F.fit_transform(adata)
+    adata = T.fit_transform(adata)
     adata.X = adata.X.toarray()
 
     label_col = args.label_class
-    if not args.no_shap:
-        model_storage = STORAGE.joinpath("shapley")
-        model_storage.mkdir(parents=True, exist_ok=True)
-        model = PredBase(model=XGBEstimator())
-        for name, fn in ADDITIONAL_SPLITS.items():
-            _, test = fn(adata)
-            unique_values = test.obs[label_col].unique()
-            spec = {label_col: [(u, 5) for u in unique_values]}
-            train, test = split_and_sample(adata, fn, spec, RNG)
-            saved_model_file = model_storage.joinpath(f"{name}_model.pkl")
-            model.fit(train)
-            # if not saved_model_file.exists():
-            # BUG: [2025-03-31 Mon] Using the loaded model results in really poor performance for some reason
-            #     write_pickle(model, saved_model_file)
-            # else:
-            #     model = load_pickle(saved_model_file)
+    for name, fn in ADDITIONAL_SPLITS.items():
+        _, test = fn(adata)
+        unique_values = test.obs[label_col].unique()
+        spec = {label_col: [(u, 5) for u in unique_values]}
+        train, test = remove_zero_features(
+            adata,
+            F,
+            model=M,  # will get fit
+            splitter=lambda x: split_and_sample(x, fn, spec, RNG),
+        )
+        for method, fn in [("shapley", shap_helper), ("anchor", anchor_helper)]:
+            if method in ignored:
+                print(f"Ignoring method {method}")
+                continue
             for n in [20, 10, 5]:
-                n_outdir = OUTDIR.joinpath("shapley").joinpath(str(n))
+                n_outdir = OUTDIR.joinpath(method).joinpath(str(n))
                 n_outdir.mkdir(exist_ok=True)
-                neg_contrib = shap_helper(
-                    model, train, test, label_col=label_col, set_name=name
+                neg_contrib = fn(
+                    model=M,
+                    train=train,
+                    test=test,
+                    label_col=label_col,
+                    set_name=name,
+                    n=n,
                 )
-                proba = model.predict_proba(test)
-                perf = get_all_metrics(test.obs[label_col], proba, model.classes_)
+                proba = M.predict_proba(test)
+                perf = get_all_metrics(test.obs[label_col], proba, M.classes_)
                 before = n_outdir.joinpath(f"{name}_before.txt")
                 write_metrics(before, perf)
 
                 # Try hard masking the negatively-contributing features
                 test.X[:, test.var["GENEID"].isin(neg_contrib)] = 0
-                proba = model.predict_proba(test)
-                perf_after = get_all_metrics(test.obs[label_col], proba, model.classes_)
+                proba = M.predict_proba(test)
+                perf_after = get_all_metrics(test.obs[label_col], proba, M.classes_)
                 after = n_outdir.joinpath(f"{name}_after.txt")
                 write_metrics(after, perf_after)
-
-            # Try removing altogether [2025-03-28 Fri] DONE
-            # new_filter = Filter(
-            #     feature_col="GENEID",
-            #     features=FEATURES[feature_set],
-            #     blacklist=neg_contrib,
-            # )
-            # no_neg_contrib = new_filter.fit_transform(adata)
-            # print(f"Old shape: {adata.shape}\nNew shape: {no_neg_contrib.shape}")
-            # # See how the model performs after removing the negatively contributing
-            # # features
-            # updated_perf = model.holdout(  # Will refit in here
-            #     no_neg_contrib,
-            #     {k: v for k, v in ADDITIONAL_SPLITS.items() if k == name},
-            #     label_col=label_col,
-            # )
-            # write_cross_val(updated_perf, OUTDIR.joinpath("shapley"), prefix=name)
 
 
 if __name__ == "__main__":
