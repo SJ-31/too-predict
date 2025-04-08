db127a240bde3527f2386e3f5ebad8d4f1c5ad10
Shann Chongwattanankul <shanj3131@gmail.com>
2025-04-08 16:35:22 +0700

    chore: add recoder to _train_utils

--- src/too_predict/_train_utils.py
+++ src/too_predict/_train_utils.py
@@ -7,14 +7,14 @@ from sklearn.tree import DecisionTreeClassifier
 
 import too_predict.model as tm
 from too_predict.filter import Filter
+from too_predict.go_utils import RecodeGO
 from too_predict.imbalance import Balancer
 from too_predict.imputer import Imputer
-from too_predict.model import PredBase, RandomForestClassifier
+from too_predict.model import PredBase, RandomForestClassifier, XGBEstimator
 from too_predict.transformer import Transformer
 from too_predict.utils import (
     RNG,
     get_blacklist_internal,
-    recode_to_go,
     ref_feature_lists_internal,
 )
 
@@ -28,8 +28,9 @@ REF_LISTS, FEATURE_LISTS = ref_feature_lists_internal()
 # f : feature set
 # r : reference set
 # e : encoding (only "GO" and None) are supported [2025-04-01 Tue]
+# b : balancer
 # k : kwargs to transformer
-# b : feature blacklist
+# l : feature blacklist
 
 MODELS: dict = {
     "clr_random_forest_minfo": {
@@ -45,6 +46,7 @@ MODELS: dict = {
         "i": "plus_one",
         "f": "edgeR_median_lfc_feature_list_3000",
         "r": "variance_feature_list_lowest_20",
+        "s": True,  # [2025-04-08 Tue]
     },
     "clr_xgboost_edger_GO": {
         "m": tm.PredBase(model=tm.XGBEstimator()),
@@ -82,6 +84,7 @@ MODELS: dict = {
         "t": "clr",
         "i": "plus_one",
         "f": "edgeR_median_lfc_feature_list_3000",
+        "s": True,  # [2025-04-08 Tue]
     },
     "clr_xgboost_edger_1000_undersample": {
         "m": tm.PredBase(model=tm.XGBEstimator()),
@@ -89,12 +92,14 @@ MODELS: dict = {
         "i": "plus_one",
         "f": "edgeR_median_lfc_feature_list_1000",
         "b": Balancer(method="RandomUnderSampler", sampling_strategy="not minority"),
+        "s": True,  # [2025-04-08 Tue]
     },
     "clr_xgb3_edger": {
         "m": tm.PredBase(model=tm.XGBEstimator(max_depth=3)),
         "t": "clr",
         "i": "plus_one",
         "f": "edgeR_median_lfc_feature_list_3000",
+        "s": True,  # [2025-04-08 Tue] Surprisingly good
     },
     "clr_xgboost_edger_1000_organoid_edger_blacklist": {
         "m": tm.PredBase(model=tm.XGBEstimator()),
@@ -109,8 +114,13 @@ MODELS: dict = {
         "i": "plus_one",
         "f": "edgeR_median_lfc_feature_list_3000",
     },
-    "batch_xgboost_rf_edger": {  # [2025-03-27 Thu] TODO: choose a fast m for `outer`
-        "m": tm.BatchBase(inner=tm.XGBEstimator(), outer_y="", outer=None),
+    "batch_xgboost_lg_edger": {  # [2025-03-27 Thu] TODO: choose a fast m for `outer`
+        "m": tm.BatchBase(
+            inner=tm.XGBEstimator(max_depth=3),
+            outer_y="Sample_Type",
+            categorical_support=False,
+            outer=tm.PredBase(RandomForestClassifier()),
+        ),
         "t": "clr",
         "i": "plus_one",
         "f": "edgeR_median_lfc_feature_list_1000",
@@ -162,8 +172,8 @@ MODELS: dict = {
             references=REF_LISTS["edgeR_median_lfc_feature_list_lowest_20"],
         ),
         "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_lowest_20",
-        "r": "variance_feature_list_lowest_20",
+        "f": "edgeR_median_lfc_feature_list_1000",
+        "r": "edgeR_median_lfc_feature_list_lowest_20",
     },
     "alr_xgboost_edger_lowest_1000_only_5": {
         "m": tm.AlrBase(
@@ -172,8 +182,23 @@ MODELS: dict = {
             n_refs=5,
         ),
         "i": "plus_one",
-        "f": "edgeR_median_lfc_feature_list_lowest_20",
-        "r": "variance_feature_list_lowest_20",
+        "f": "edgeR_median_lfc_feature_list_1000",
+        "r": "edgeR_median_lfc_feature_list_lowest_20",
+    },
+    "clr_xgboost_go_level_4_sum": {
+        "m": PredBase(XGBEstimator(max_depth=3)),
+        "i": "plus_one",
+        "e": RecodeGO(id_col="GENEID", level=4),
+    },
+    "clr_xgboost_go_level_3_sum": {
+        "m": PredBase(XGBEstimator(max_depth=3)),
+        "i": "plus_one",
+        "e": RecodeGO(id_col="GENEID", level=3),
+    },
+    "clr_xgboost_go_level_2_sum": {
+        "m": PredBase(XGBEstimator(max_depth=3)),
+        "i": "plus_one",
+        "e": RecodeGO(id_col="GENEID", level=2),
     },
     "alr_random_forest_low_variance": {
         "m": tm.AlrBase(
@@ -256,29 +281,30 @@ ADDITIONAL_SPLITS: dict = {
 # * Helper functions
 def read_model_spec(
     spec: dict,
-) -> tuple[Filter, PredBase, Transformer, Balancer | None, Callable]:
+) -> tuple[Filter | None, PredBase, Transformer, Balancer | None, RecodeGO | None]:
     M: PredBase = spec.get("m")
     references = spec.get("r")
     features = spec.get("f")
     encoding = spec.get("e")
     blacklist = spec.get("l")
 
-    if encoding == "GO":
-        encode_fn = lambda x: recode_to_go(x)
-        fcol = "GO accession"
+    if encoding is not None:
+        fcol = "accession"
     else:
-        encode_fn = lambda x: x
         fcol = "GENEID"
     b_list = None
     if blacklist is not None:
-        b_list = get_blacklist_internal(b_list)
-    f_list = FEATURE_LISTS[features]
+        b_list = get_blacklist_internal(blacklist)
+    f_list = FEATURE_LISTS.get(features)
     r_list = REF_LISTS.get(references)
-    F = Filter(
-        f_list if references is None else f_list + r_list,
-        feature_col=fcol,
-        blacklist=b_list,
-    )
+    if f_list:
+        F = Filter(
+            f_list if references is None else f_list + r_list,
+            feature_col=fcol,
+            blacklist=b_list,
+        )
+    else:
+        F = None
     B: Balancer = spec.get("b")
     transformation_name = spec.get("t")
     kwargs: dict = spec.get("k", {})
@@ -287,4 +313,4 @@ def read_model_spec(
     T = Transformer(
         transformation_name, impute_fn=Imputer(spec.get("i")), inplace=False, **kwargs
     )
-    return F, M, T, B, encode_fn
+    return F, M, T, B, encoding
