e69f3105b0db613291cb2e16f0a007b0e7978576
Shann Chongwattanankul <shanj3131@gmail.com>
2025-04-28 16:57:12 +0700

    chore: try out using simplified model for feature selection

--- scripts/feature_selection_organoid.py
+++ scripts/feature_selection_organoid.py
@@ -7,29 +7,26 @@ import joblib
 import pandas as pd
 import scanpy as sc
 import sklearn.feature_selection as fs
-import sklearn.inspection as si
 import sklearn.metrics as sm
-from dask.distributed import Client
-from dask_jobqueue import SLURMCluster
 from pyhere import here
-from sklearn.ensemble import HistGradientBoostingClassifier, RandomForestClassifier
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.model_selection import StratifiedKFold, cross_val_score, train_test_split
+from too_predict._train_utils import ADDITIONAL_SPLITS, MODELS, read_model_spec
+from too_predict.evaluation import write_cross_val
 from too_predict.filter import Filter
-from too_predict.imputer import Imputer
-from too_predict.transformer import Transformer
+from too_predict.model import PredBase
 from too_predict.utils import (
     RANDOM_STATE,
-    RNG,
-    ref_feature_lists_internal,
+    train_test_split_ad,
     training_data_internal,
     training_data_internal_test,
 )
 
 OUTDIR: Path = here("data", "output", "organoid_feature_selection")
 OUTDIR.mkdir(exist_ok=True, parents=True)
-REF_LISTS, FEATURE_LISTS = ref_feature_lists_internal()
-
 TEST = True
+
+F, M, T, B, R, C = read_model_spec(MODELS["clr_xgb3_edger"])
 
 
 def parse_args():
@@ -44,47 +41,79 @@ def parse_args():
     return parser.parse_args()
 
 
-def main():
+def get_adata() -> tuple[ad.AnnData, ad.AnnData]:
     if TEST:
         adata = training_data_internal_test()
     else:
         adata = training_data_internal()
 
-    model = RandomForestClassifier()
-
-    filter = Filter(
-        feature_col="GENEID",
-        features=FEATURE_LISTS["edgeR_median_lfc_feature_list_3000"],
+    adata.obs["is_organoid"] = adata.obs["Sample_Type"] == "organoid"
+    primary = adata.copy()[~adata.obs["is_organoid"], :]
+    sc.pp.subsample(primary, random_state=RANDOM_STATE, fraction=0.03)
+    organoid = adata.copy()[adata.obs["is_organoid"], :]
+    organoid_primary = ad.concat(
+        [primary, organoid], axis="obs", join="inner", merge="same"
     )
-    adata = adata[adata.obs["Sample_Type"].isin(["organoid", "primary"])]
-    adata = filter.fit_transform(adata)
+
+    adata = T.fit_transform(adata)
+    organoid_primary: ad.AnnData = T.fit_transform(organoid_primary)
+    adata: ad.AnnData = adata[adata.obs["Sample_Type"].isin(["organoid", "primary"])]
     if TEST:
         adata = adata[:50, :50]
+    return adata, organoid_primary
 
-    Transformer("clr", impute_fn=Imputer("plus_one"), inplace=True).fit_transform(adata)
-    adata.obs["is_organoid"] = adata.obs["Sample_Type"] == "organoid"
-    primary = adata[~adata.obs["is_organoid"], :]
-    sc.pp.subsample(primary, random_state=RANDOM_STATE, fraction=0.03)
-    organoid = adata[adata.obs["is_organoid"], :]
 
-    adata = ad.concat([primary, organoid], axis="obs", join="inner", merge="same")
-    print(f"Shape after balancing {adata.shape}")
-    labels = adata.obs["is_organoid"]
-    counts = adata.X.toarray()
+def main2():
+    # Identify important features on a organoid vs primary prediction task using
+    # a simple model, and see if top 3000 least important features from this task
+    # are still useful for predicting tumor type
+    adata, organoid_primary = get_adata()
+    o_model: PredBase = PredBase(model=LogisticRegressionCV())
+    organoid_prediction_scores = o_model.cross_validate(
+        organoid_primary, label_col="is_organoid"
+    )
+    write_cross_val(organoid_prediction_scores, OUTDIR, prefix="organoid_prediction")
+    o_train, o_test = train_test_split_ad(organoid_primary)
+    o_model.fit(o_train, y="is_organoid")
+    with_coefs = pd.Series(o_model.model.coef_[0], index=organoid_primary.var["GENEID"])
+    with_coefs = with_coefs[~with_coefs.index.isna()]
+    with_coefs = with_coefs.abs().sort_values(ascending=True)
+    lowest_n = with_coefs[:3000]
+    new_filter = Filter(features=lowest_n.index, feature_col="GENEID")
+    print(adata)
+    print(len(new_filter.features))
+    print(adata.shape)
+    adata = new_filter.fit_transform(adata)
+    results = M.cross_validate(adata, label_col="tumor_type")
+
+    write_cross_val(results, OUTDIR, prefix="after_removing")
+    results2 = M.holdout(adata, ADDITIONAL_SPLITS, label_col="tumor_type")
+    write_cross_val(results2, OUTDIR, prefix="after_removing_holdout")
+
+
+def main():
+    adata, organoid_primary = get_adata()
+
+    adata = F.fit_transform(adata)
+    organoid_primary = F.fit_transform(organoid_primary)
+    labels = organoid_primary.obs["is_organoid"]
+    counts = organoid_primary.X.toarray()
     scorer = sm.make_scorer(sm.cohen_kappa_score)
     x_train, x_test, y_train, y_test = train_test_split(counts, labels)
-    model.fit(x_train, y_train)
-    adata.var.loc[:, "raw_importance"] = model.feature_importances_
-    adata.var.to_csv(OUTDIR.joinpath("raw_importances.csv"), index=False)
+    M.fit(x_train, y_train)
+    organoid_primary.var.loc[:, "raw_importance"] = M.feature_importances_
+    organoid_primary.var.to_csv(OUTDIR.joinpath("raw_importances.csv"), index=False)
 
     # [2025-03-13 Thu] Results were all zero with permutation_importance
-    rfecv = fs.RFECV(estimator=model, step=1, cv=StratifiedKFold(5), scoring=scorer)
+    rfecv = fs.RFECV(estimator=M, step=1, cv=StratifiedKFold(5), scoring=scorer)
     rfecv.fit(counts, labels)
 
-    cv_score = cross_val_score(model, x_train, y_train)
+    cv_score = cross_val_score(M, x_train, y_train)
     print(cv_score)
 
-    df = pd.DataFrame({"GENEID": adata.var["GENEID"], "ranking": rfecv.ranking_})
+    df = pd.DataFrame(
+        {"GENEID": organoid_primary.var["GENEID"], "ranking": rfecv.ranking_}
+    )
     df.to_csv(OUTDIR.joinpath("importances.csv"), index=False)
     score_df = pd.DataFrame(rfecv.cv_results_)
     score_df.to_csv(OUTDIR.joinpath("cv_results.csv"), index=False)
@@ -93,10 +122,8 @@ def main():
 if __name__ == "__main__":
     args = parse_args()
     TEST = args.test
-    if args.dask:
-        cluster = SLURMCluster(cores=int(args.cores), memory=f"{args.memory} GB")
-        client = Client(cluster)
     backend = "dask" if args.dask else "loky"
-    par_args = {"wait_for_workers_timeout": 0} if args.dask else {"n_jobs": args.cores}
+    par_args = {"n_jobs": args.cores}
     with joblib.parallel_backend(backend, **par_args):
-        main()
+        # main()
+        main2()
--- scripts/run/feature_selection_o.bash
+++ scripts/run/feature_selection_o.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-srun --qos=cpu24h --nodes=1 --ntasks=1 --cpus-per-task=8 --mem=30G --mail-type=end \
+srun --qos=cpu24h --nodes=1 --ntasks=1 --cpus-per-task=8 --mem=50G --mail-type=end \
     --mail-user=shanj3131@gmail.com --job-name=organoid_features \
-    python ./feature_selection_organoid.py -c 8
+    python ./feature_selection_organoid.py -c 8 -m 50
