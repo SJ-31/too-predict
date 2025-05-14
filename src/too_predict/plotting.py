0f5bac6242118e5ba2537ea5950a5557169a1806
Shann Chongwattanankul <shanj3131@gmail.com>
2025-05-14 16:10:46 +0700

    chore: add new heatmap plot with marsilea

--- src/too_predict/plotting.py
+++ src/too_predict/plotting.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env ipython
 from collections.abc import Iterable
+from typing import Literal, Sequence, overload
 
 import anndata as ad
+import marsilea as ma
+import marsilea.plotter as mp
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import scanpy as sc
+import scipy.sparse as sparse
 import seaborn as sns
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
+from pypalettes import load_cmap
 
 
 def plot_diagonal_matrix(matrix: pd.DataFrame, ax: Axes, **kwargs) -> None:
@@ -178,3 +183,183 @@ def plot_adata(
                 set_labels(ax)
     fig.suptitle(subtitle)
     return fig
+
+
+def scanpy_plot_gs(
+    adata: ad.AnnData,
+    genes: Sequence,
+    grouping_var: str | None = None,
+    gene_sets: dict[str, set] | None = None,
+    gene_set_limit: int = 5,
+    method: Literal["heatmap", "tracksplot"] = "tracksplot",
+    gene_symbols: str | None = None,
+    gene_symbols_to_show: str | None = None,
+    **kwargs,
+):
+    """Convenience function for creating mappings for scanpy plots
+
+    Parameters
+    ----------
+    genes : list of genes to plot
+    gene_sets : dict defining gene set groupings to plot. Genes in this dict must
+        use the same identifiers as in `genes`
+    grouping_var : a column in adata.var to be used to group genes
+    gene_symbols : column in adata.var corresponding to gene ids. Defaults to adata.var.index
+    gene_symbols_to_show : column in adata.var to show in the plot instead of
+        gene_symbols. The mapping is performed internally
+    -------
+    """
+    var: pd.DataFrame = adata.var
+    mapping_to_plot: dict = {}
+    id_mapping = {}
+
+    if gene_symbols_to_show is not None:
+        for gene in genes:
+            try:
+                if gene_symbols is None:
+                    id_mapping[gene] = var[gene_symbols_to_show][var.index == gene][0]
+                else:
+                    id_mapping[gene] = var[gene_symbols_to_show][
+                        var[gene_symbols] == gene
+                    ][0]
+            except IndexError:
+                id_mapping[gene] = None
+
+    def map_if(genes, filter: set | None):
+        # Only allow genes present in `filter` if it is provided
+        if id_mapping and filter is not None:
+            return [id_mapping[g] for g in genes if g in filter]
+        elif id_mapping:
+            return [id_mapping[g] for g in genes]
+        elif filter is not None:
+            return [g for g in genes if g in filter]
+        return genes
+
+    as_set: set = set(genes)
+    if gene_sets is not None:
+        for k, v in gene_sets.items():
+            if len(v & as_set) == 0:
+                continue
+            mapping_to_plot[k] = map_if(v, as_set)
+            if len(mapping_to_plot) >= gene_set_limit:
+                break
+    elif grouping_var is not None:
+        for group in adata.var[grouping_var].unique():
+            subset = adata.var.loc[adata.var[grouping_var] == group, :]
+            if gene_symbols is None:
+                subset_g = subset.index[subset.index.isin(as_set)]
+            else:
+                subset_g = subset[gene_symbols][subset[gene_symbols].isin(as_set)]
+            mapping_to_plot[group] = map_if(subset_g, None)
+
+    if method == "heatmap":
+        sc.pl.heatmap(
+            adata, mapping_to_plot, gene_symbols=gene_symbols_to_show, **kwargs
+        )
+    elif method == "tracksplot":
+        sc.pl.tracksplot(
+            adata, mapping_to_plot, gene_symbols=gene_symbols_to_show, **kwargs
+        )
+
+
+@overload
+def rand_cmap_d(val: int | None, assign: bool = True) -> list[str]: ...
+
+
+@overload
+def rand_cmap_d(val: Sequence, assign: bool = False) -> dict: ...
+
+
+def rand_cmap_d(
+    val: Sequence | None | int = None, assign: bool = False
+) -> list[str] | dict:
+    def get_n(length: int) -> list[str]:
+        tmp = set()
+        while len(tmp) < length:
+            tmp |= set(load_cmap().colors)
+        return list(tmp)[:length]
+
+    if val is None:
+        return load_cmap().colors
+    if isinstance(val, int):
+        return get_n(val)
+    else:
+        uniques = set(val)
+        colors = get_n(len(uniques))
+        mapping = dict(zip(uniques, colors))
+        if assign:
+            return [mapping[v] for v in val]
+        return mapping
+
+
+def mp_plot(
+    adata: ad.AnnData,
+    genes: Sequence,
+    var_groupings: str | None | Sequence = None,
+    method: Literal["heatmap", "tracksplot"] = "tracksplot",
+    gene_symbols: str | None = None,
+    gene_symbols_to_show: str | None = None,
+    sample_groupings: str | None | Sequence = None,
+    cmaps: dict | None = None,
+    var_spacing: float = 0.001,
+    obs_spacing: float = 0.001,
+    **kwargs,
+):
+    expr: np.ndaarray = adata.X.toarray() if sparse.issparse(adata.X) else adata.X
+    cmaps = {} if cmaps is None else cmaps
+    var: pd.DataFrame = adata.var
+    vmask: np.ndarray = (
+        var.index.isin(genes) if gene_symbols is None else var[gene_symbols].isin(genes)
+    )
+    expr = expr[:, vmask]
+    if gene_symbols_to_show is not None:
+        labels = mp.Labels(var[gene_symbols_to_show][vmask])
+    else:
+        labels = mp.Labels(genes)
+    if method == "heatmap":
+        m = ma.Heatmap(expr, **kwargs)
+        m.add_top(labels, pad=0.1)
+    else:
+        raise ValueError("Not implemented yet!")
+
+    # Annotate genes
+    if var_groupings is not None and isinstance(var_groupings, str):
+        var_groupings = [var_groupings]
+    if var_groupings is not None:
+        vals = list(var[var_groupings[0]][vmask])
+        v_order = set(vals)
+        v_cmap: dict = cmaps.get("grouping_var", rand_cmap_d(v_order))
+        var_chunk = mp.Chunk(
+            list(v_order),
+            fill_colors=[v_cmap[g] for g in v_order],
+            rotation=90,
+            align="center",
+        )
+        m.add_top(var_chunk, pad=0.1)
+        m.group_cols(vals, order=v_order, spacing=var_spacing)
+    if var_groupings is not None and len(var_groupings) > 1:
+        for vals in var_groupings[1:]:
+            values = list(adata.var[vals])
+            cmap = cmaps.get(vals, rand_cmap_d(values))
+            colors = mp.Colors(values, palette=cmap, label=vals, label_loc=None)
+            m.add_top(colors, size=0.2, pad=0.1)
+
+    # Annotate samples
+    if sample_groupings is not None and isinstance(sample_groupings, str):
+        sample_groupings = [sample_groupings]
+    if sample_groupings is not None:
+        svals = list(adata.obs[sample_groupings[0]])
+        order = set(svals)
+        chnk = mp.Chunk(list(order), rotation=0, align="right")
+        m.add_left(chnk, pad=0.1)
+        m.group_rows(svals, order=order, spacing=obs_spacing)
+    if sample_groupings is not None and len(sample_groupings) > 1:
+        for svals in sample_groupings[1:]:
+            values = list(adata.obs[svals])
+            cmap = cmaps.get(svals, rand_cmap_d(values))
+            colors = mp.Colors(values, palette=cmap, label=svals, label_loc=None)
+            m.add_left(colors, size=0.2, pad=0.1)
+
+    m.add_legends()
+    m.render()
+    return m
