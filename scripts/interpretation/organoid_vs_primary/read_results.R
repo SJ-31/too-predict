300b503f6021f33a0f6ca921fec512e765d4a22e
Shann Chongwattanankul <shanj3131@gmail.com>
2025-05-16 14:58:42 +0700

    chore: add upset plots and chi-square

--- scripts/interpretation/organoid_vs_primary/read_results.R
+++ scripts/interpretation/organoid_vs_primary/read_results.R
@@ -2,8 +2,10 @@ library(tidyverse)
 library(limma)
 library(glue)
 library(here)
+library(broom)
 library(igraph)
 library(ComplexHeatmap)
+source(here("src", "R", "utils.R"))
 
 outdir_o <- here("data", "output", "chula_organoid_comparison", "de_enrichment")
 contrasts <- list(
@@ -57,22 +59,36 @@ top_tags <- sapply(names(contrasts), \(n) {
     filter(PValue <= 0.05 & !is.na(GENEID))
 }, simplify = FALSE, USE.NAMES = TRUE)
 
-batch_var <- read_tsv(here("data", "output", "explanations", "batch_correction", "var.csv"))
+batch_var <- read_csv(here("data", "output", "explanations", "batch_correction", "var.csv"))
 
+top_tags$sample_type %>%
+  arrange(desc(abs(logFC))) |>
+  slice(1:1000) |>
+  pluck("GENEID") |>
+  writeLines(here("data", "output", "feature_selection", "blacklists", "organoid_vs_primary_lfc-1000.txt"))
 
-n <- 1000
+get_to_upset <- function(tb_list, target_col, direction, direction_col = "logFC") {
+  sapply(tb_list, \(x) {
+    if (direction == "pos") {
+      cur <- x[x[[direction_col]] > 0, ]
+    } else if (direction == "neg") {
+      cur <- x[x[[direction_col]] < 0, ]
+    } else {
+      stop("Direction must be 'pos' or 'neg'")
+    }
+    cur[[target_col]]
+  }, simplify = FALSE, USE.NAMES = TRUE)
+}
 
+# Upset plot of top most downreg DE genes
+to_upset_pos <- get_to_upset(list_modify(top_tags, sample_type = zap()), "GENENAME", "pos")
+to_upset_neg <- get_to_upset(list_modify(top_tags, sample_type = zap()), "GENENAME", "neg")
+mt_pos <- make_comb_mat(to_upset_pos)
+mt_neg <- make_comb_mat(to_upset_neg)
 
-# Upset plot of top 1000 most downreg DE genes
-to_upset <- sapply(list_modify(top_tags, sample_type = zap()), \(x) {
-  x |>
-    filter(logFC < 0) |>
-    pluck("GENENAME")
-}, simplify = FALSE, USE.NAMES = TRUE)
-
-mt <- make_comb_mat(to_upset)
-png(here(outdir_o, "downreg_de.png"), width = 8, height = 8, units = "in", res = 1080)
-UpSet(mt)
+png(here(outdir_o, "de_gene_upset.png"), width = 10, height = 15, units = "in", res = 1080)
+UpSet(mt_pos, row_title = "Upregulated DE genes") %v%
+  UpSet(mt_neg, row_title = "Downregulated DE genes")
 dev.off()
 
 # Shared characteristics of uniquely downreg genes
@@ -90,4 +106,66 @@ ud_families <- table(unique_downreg$tumor_type, unique_downreg$family)
 top_tags_all <- list_modify(top_tags, sample_type = zap()) |>
   lmap(\(x) mutate(x[[1]], tumor_type = names(x[1]))) |>
   bind_rows()
-fcounts <- table(top_tags_all$tumor_type, top_tags_all$family)
+
+# TODO: you should include only the genes that are not DE between any of the tumor
+# types in primary samples. Then you'd know that the significant famlies below
+# are DE as a result of the organoid treatment
+chi_square_fams <- read_existing(here(outdir_o, "chisq_org_families.rds"), \(f) {
+  results <- list()
+  directions <- list(
+    downregulated = filter(top_tags_all, logFC < 0),
+    upregulated = filter(top_tags_all, logFC > 0)
+  )
+  fams <- unique(top_tags_all$family)
+  for (n in names(directions)) {
+    test_fams <- lapply(fams, \(fam) {
+      tab <- table(directions[[n]]$tumor_type, directions[[n]]$family == fam)
+      if (dim(tab)[2] == 1) {
+        return(NULL)
+      }
+      tb <- table2tb(tab, fam)
+      tab |>
+        chisq.test() |>
+        tidy() |>
+        mutate(family = fam, data = list(tb))
+    }) |>
+      bind_rows() |>
+      mutate(padj = p.adjust(p.value))
+    results[[n]] <- test_fams
+  }
+  saveRDS(results, f)
+  results
+}, readRDS)
+
+
+
+## * BC
+bc_top_tags <- inner_join(top_tags$sample_type, batch_var, by = join_by(GENEID))
+bc_corr <- cor.test(log(bc_top_tags$bc_mean_organoid_fc), bc_top_tags$logFC)
+
+corr_string <- glue("Correlation: {round(bc_corr$estimate, 3)}")
+bc_corr_plot <- ggplot(bc_top_tags, aes(x = logFC, y = log(bc_mean_organoid_fc), color = GENEBIOTYPE)) +
+  geom_point() +
+  ylab("Mean lfc of organoid samples after correction") +
+  labs(
+    title = "Correlation between combat-ref batch correction and lfc",
+    subtitle = corr_string
+  )
+ggsave(here(outdir_o, "bc_fc_correlation.png"), bc_corr_plot, width = 13)
+
+
+## * Gene set overlaps
+
+fgsea <- sapply(names(contrasts), \(x) {
+  read_tsv(here(outdir_o, "gene_sets", glue("fgsea_{x}.tsv"))) |>
+    mutate(direction = as.numeric(case_match(direction, "pos" ~ 1, "neg" ~ -1)))
+}, simplify = FALSE, USE.NAMES = TRUE)
+
+to_upset_pos <- get_to_upset(list_modify(fgsea, sample_type = zap()), "pathway", "pos", "direction")
+to_upset_neg <- get_to_upset(list_modify(fgsea, sample_type = zap()), "pathway", "neg", "direction")
+## mt_pos <- make_comb_mat(to_upset_pos)
+# [2025-05-16 Fri] Only two pathways are upregulated
+mt_neg <- make_comb_mat(to_upset_neg)
+png(here(outdir_o, "de_pathway_upset.png"), width = 8, height = 8, units = "in", res = 1080)
+UpSet(mt_neg, row_title = "Downregulated pathways")
+dev.off()
