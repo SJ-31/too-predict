217d61c38d4af0e574523aec35108eebcb391c4c
Shann Chongwattanankul <shanj3131@gmail.com>
2025-07-04 13:24:16 +0700

    feat: add script for estimating intrinsic dimension

---
+++ scripts/deep/intrinsic_dimension.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env ipython
+
+from pathlib import Path
+
+import anndata as ad
+import pandas as pd
+import skdim
+import too_predict.utils as ut
+from pyhere import here
+from too_predict.filter import Filter
+from too_predict.imputer import Imputer
+from too_predict.transformer import Transformer
+
+OUTDIR: Path = here("data", "output", "deep")
+TRANSFORM: Transformer = Transformer(
+    "clr", impute_fn=Imputer("plus_one"), inplace=False
+)
+REF, FEAT = ut.ref_feature_lists_internal()
+
+
+def parse_args():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input")
+    parser.add_argument("-o", "--output")
+    parser.add_argument("-t", "--test", default=False, help="Test", action="store_true")
+    args = vars(parser.parse_args())  # convert to dict
+    return args
+
+
+@ut.SaveOrLoad(
+    out=here(OUTDIR, "id_estimation.csv"), read_fn=pd.read_csv, logdir=here("log")
+)
+def get_id(f, x: ad.AnnData, filters: list[str]):
+    results = {"MoM": [], "MLE": [], "feature_set": []}
+    for ref in filters:
+        if ref != "all":
+            F = Filter(features=FEAT[ref], inplace=False, feature_col="GENEID")
+            current = F.fit_transform(x)
+        else:
+            current = x.copy()
+        mle = skdim.id.MLE()
+        mle.fit(current)
+        results["MLE"].append(mle.dimension_)
+        mom = skdim.id.MOM()
+        mom.fit(current)
+        results["MoM"].append(mom.dimension_)
+        results["feature_set"].append(ref)
+    df = pd.DataFrame(results)
+    df.to_csv(f, index=False)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    adata: ad.AnnData
+    if args["test"]:
+        print("Using test subset")
+        adata = ut.training_data_internal_test(minimal=False)
+        OUTDIR = OUTDIR.joinpath("test")
+        OUTDIR.mkdir(exist_ok=True, parents=True)
+    else:
+        adata = ut.training_data_internal()
+    adata = TRANSFORM.fit_transform(adata)
+    adata.X = adata.X.toarray()
+    get_id(
+        x=adata,
+        filters=[
+            "all",
+            "edgeR_median_lfc_feature_list_3000",
+            "edgeR_15_per_type_ovp_tissue_enriched",
+            "edgeR_70_per_type_ovp_.txt",
+            "variance_feature_list_3000",
+        ],
+    )
