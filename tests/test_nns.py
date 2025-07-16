0a7fccd17dafdffeae4533e98d57b2f8bcce3cf8
Shann Chongwattanankul <shanj3131@gmail.com>
2025-07-16 10:50:04 +0700

    chore: update tests and pyproject

--- tests/test_nns.py
+++ tests/test_nns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env ipython
 
 import anndata as ad
+import lightning as L
 import numpy as np
 import scipy.spatial.distance as spd
 import sklearn.datasets as datasets
@@ -19,9 +20,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.optim.lr_scheduler as schedule
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from pyhere import here
 from too_predict.deep.evaluation import multitask_acc, train_test_split_torch
 from too_predict.deep.logistic import DummyLR, MtcLr, MultiLevel
+from too_predict.deep.trainer import Trainer
 from too_predict.imputer import Imputer
 from torch.utils.data import DataLoader
 
@@ -29,7 +32,7 @@ cache = here("data", ".sklearn")
 # %%
 
 
-torch.set_default_dtype(torch.float64)
+torch.set_default_dtype(torch.float32)
 
 
 adata = ut.training_data_internal_test(minimal=True)  # 1000 features
@@ -47,35 +50,61 @@ n_features, n_classes = d_ut.data_spec(train_l)
 base = d_ev.Baseline(n_features, n_classes, max_depth=1)
 base.fit(*train_l.dataset[:])
 res = base.predict(test_l.dataset[:][0])
-base_acc = d_ev.multitask_acc(test_l.dataset[:][1], res, ["Sample_Type", "tumor_type"])
+base_acc = d_ev.multitask_acc(
+    test_l.dataset[:][1],
+    res,
+    task_names=["Sample_Type", "tumor_type"],
+    n_classes=n_classes,
+)
 print(f"Base acc: {base_acc}")
 
 # %%
 #
+EPOCHS = 10
 
 
 def test_disyak():
-    model = d_nn.Disyak(
-        n_features,
-        n_classes_per_task=n_classes,
-        reduce_features=False,
-    )
+    model = d_nn.Disyak(n_features, n_classes_per_task=n_classes, record_metrics=False)
     optimizer = optim.Adam(model.named_parameters(), lr=0.001)
     sch = schedule.ReduceLROnPlateau(optimizer=optimizer, patience=40)
-    trainer = d_ut.Trainer(
+    trainer = Trainer(
         model,
         optimizer=optimizer,
-        n_epochs=10,
+        n_epochs=EPOCHS,
         record_test_score=False,
         at_batch_level=True,
         scheduler=sch,
     )
-    result = trainer(train_l)
+    result = trainer(train_l, n_classes=n_classes)
     acc = d_ev.multitask_acc(
-        test_l.dataset[:][1], model.predict(test_l), ["Sample_Type", "tumor_type"]
+        test_l.dataset[:][1],
+        model.predict_step(test_l),
+        task_names=["Sample_Type", "tumor_type"],
+        n_classes=n_classes,
     )
     print(f"Disyak acc: {acc}")
     return model, result
 
 
-disyak, res = test_disyak()
+# disyak, time = d_ut.timed(lambda: test_disyak())
+
+
+def test_lightning():
+    model = d_nn.Disyak(n_features, n_classes_per_task=n_classes, record_metrics=True)
+    trainer = L.Trainer(
+        max_epochs=EPOCHS,
+        log_every_n_steps=2,
+        enable_progress_bar=False,
+        enable_checkpointing=False,
+        callbacks=[
+            EarlyStopping(
+                monitor="train_loss", patience=10, mode="min"
+            ),  # If "min", lower is better
+        ],
+    )
+    trainer.fit(model=model, train_dataloaders=train_l, val_dataloaders=None)
+    trainer.test(model=model, dataloaders=test_l)
+    return model, trainer
+
+
+_, time = d_ut.timed(lambda: test_lightning())
