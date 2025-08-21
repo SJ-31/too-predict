64cf4835b6fe68d52c19a657ffdb0b5b4cd8546f
Shann Chongwattanankul <shanj3131@gmail.com>
2025-08-21 09:54:02 +0700

    feat: finalize cvae

--- src/too_predict/deep/augmentation.py
+++ src/too_predict/deep/augmentation.py
@@ -9,6 +9,8 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 from torch.distributions import Distribution
+from torch.nn.functional import one_hot
+from torch.utils.data import TensorDataset
 
 """
 [1] Polepalli, Vinil. A Novel cVAE-Augmented Deep Learning Framework for Pan-Cancer RNA-Seq Classification. 2025, https://arxiv.org/abs/2508.02743.
@@ -34,12 +36,16 @@ class GaussianApprox(DistModule):
         super().__init__(*args, **kwargs)
         self.mu: nn.LazyLinear = nn.LazyLinear(out_features=n_out)
         self.var: nn.LazyLinear = nn.LazyLinear(out_features=n_out)
+        # self.internal: nn.LazyLinear = nn.LazyLinear(out_features=n_out)
 
     @override
     def forward(self, X):
         mu_hat, var_hat = self.mu(X), self.var(X)
+        # print(var_hat.shape)
+        # mu_hat, logvar = torch.chunk(self.internal(X), 2, dim=-1)
+        scale = nn.functional.softplus(var_hat) + 1e-8
         dist = torch.distributions.MultivariateNormal(
-            mu_hat, scale_tril=torch.diag_embed(var_hat)
+            mu_hat, scale_tril=torch.diag_embed(scale)
         )
         z = dist.rsample()
         return z, dist
@@ -67,14 +73,11 @@ class BaseVAE(d_ut.BaseNN):
         self.approx_posterior: nn.Module = approx_posterior
         self.prior: Distribution | None = prior
 
-        self.cache: dict[str, tuple[bool, list]] = {
-            "train_loss": (False, []),
-            "train_acc": (False, []),  # TODO: what metrics could you report instead?
-            "val_acc": (False, []),
-            "val_loss": (False, []),
-            "test_loss": (False, []),
-            "test_acc": (False, []),
-        }
+        self.cache: dict[str, tuple[bool, list]] = {}
+        for context in ("train", "test", "val"):
+            self.cache[f"{context}_loss"] = (False, [])
+            self.cache[f"{context}_recon_loss"] = (False, [])
+            self.cache[f"{context}_dkl"] = (False, [])
         if isinstance(self.conf.cache, str):
             self.set_cache(self.conf.cache)
         elif self.conf.cache is not None:
@@ -85,7 +88,7 @@ class BaseVAE(d_ut.BaseNN):
     def training_step(self, batch, batch_idx):
         x, y = batch
         z, dist = self(x)  # Latent variable and posterior distribution
-        decoded = self.decode(z, y)
+        decoded = self.decode((z, y))
         return self.criterion(
             x_pred=decoded, x_true=x, y_true=y, dist=dist, context="train"
         )
@@ -99,8 +102,10 @@ class BaseVAE(d_ut.BaseNN):
         context: str | None = None,
         dist: Distribution | None = None,
     ):
-        kl_div = torch.distributions.kl.kl_divergence(dist, self.prior)
+        kl_div = torch.distributions.kl.kl_divergence(dist, self.prior).mean()
         recon_loss = nn.functional.mse_loss(x_pred, x_true)
+        self.log(f"{context}_recon_loss", recon_loss)
+        self.log(f"{context}_dkl", kl_div)
         return kl_div + recon_loss
 
     @override
@@ -114,7 +119,7 @@ class BaseVAE(d_ut.BaseNN):
         z, dist = self.approx_posterior(encoded)
         return z, dist
 
-    def decode(self, X: Tensor | None = None, y: Tensor | None = None):
+    def decode(self, batch: tuple[Tensor, ...]):
         "Map from latent distribution to approximate inputs, optionally with class labels"
         raise NotImplementedError()
 
@@ -126,12 +131,21 @@ class BaseVAE(d_ut.BaseNN):
     def validation_step(self, *args: Any, **kwargs: Any):
         return super().validation_step(*args, **kwargs)
 
-    @override
-    def test_step(self, *args: Any, **kwargs: Any):
-        return super().test_step(*args, **kwargs)
+    # @override
+    # def test_step(self, batch, batch_idx):
+    #     return super().test_step(*args, **kwargs)
 
-    def sample(self, **kwargs):
-        return self.decode(self.prior.rsample())
+    def from_prior(self, n: int) -> Tensor:
+        "Randomly draw `n` samples from prior"
+        return self.prior.rsample((n,))
+
+    def make_one_hot(self, labels: Tensor) -> tuple[Tensor, ...]:
+        """Create a one-hot vector from labels, accounting for class counts"""
+        encoded = [
+            one_hot(labs, count)
+            for labs, count in zip(d_ut.iter_cols(labels), self.n_classes)
+        ]
+        return tuple(encoded)
 
 
 class cVAE(BaseVAE):
@@ -152,10 +166,8 @@ class cVAE(BaseVAE):
             conf=conf,
             approx_posterior=GaussianApprox(n_out=n_latent),
         )
-        self.prior: Distribution | None = (
-            torch.distributions.MultivariateNormal(
-                torch.zeros(n_latent), torch.eye(n_latent)
-            ),
+        self.prior: Distribution | None = torch.distributions.MultivariateNormal(
+            torch.zeros(n_latent), torch.eye(n_latent)
         )
         stacked_in_features = in_features + sum(
             n_classes_per_task
@@ -181,19 +193,31 @@ class cVAE(BaseVAE):
     @override
     def training_step(self, batch, batch_idx):
         x, y = batch
-        one_hots = [nn.functional.one_hot(y_i) for y_i in d_ut.iter_cols(y)]
+        one_hots = self.make_one_hot(y)
         input = torch.cat([x, *one_hots], dim=1)
         z, dist = self(input)
-        decoded = self.decode(z, y)
-        return self.criterion(
+        decoded = self.decode((z, y))
+        loss = self.criterion(
             x_pred=decoded, x_true=x, y_true=y, dist=dist, context="train"
         )
+        self.log("train_loss", loss)
+        return loss
 
     @override
-    def decode(self, X: Tensor | None = None, y: Tensor | None = None):
-        one_hots = [nn.functional.one_hot(y_i) for y_i in d_ut.iter_cols(y)]
+    def decode(self, batch):
+        X, y = batch
+        one_hots = self.make_one_hot(y)
         decoded = self.decoder(torch.cat([X, *one_hots], dim=1))
         return decoded
+
+    def sample(self, labels: Tensor):
+        x = self.from_prior(n=labels.shape[0])
+        decoded: Tensor = self.decode((x, labels))
+        return decoded.detach()
+
+    def sample_to_dataset(self, labels: Tensor):
+        x = self.sample(labels)
+        return TensorDataset(x, labels)
 
 
 # TODO: you could use an NB prior
