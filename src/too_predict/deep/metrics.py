cba17328d4b7fb8063ce5a0a57ec568c02c69863
Shann Chongwattanankul <shanj3131@gmail.com>
2025-08-22 14:38:07 +0700

    feat: add class to handle confusion matrices across folds

--- src/too_predict/deep/metrics.py
+++ src/too_predict/deep/metrics.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env ipython
 
 from collections.abc import Sequence
+from functools import reduce
 
 import lightning as L
 import numpy as np
 import pandas as pd
+import sklearn.preprocessing as sp
 import too_predict.deep.torch_utils as d_ut
 import torch
 import torch.nn as nn
@@ -130,3 +132,110 @@ def multitask_cross_entropy_loss(
     if weights is not None and len(weights) == len(losses):
         losses = losses * weights
     return losses.sum()
+
+
+class ConfusionMatrices:
+    "Class for performing operations on a collection of confusion matrices"
+
+    def __init__(
+        self,
+        matrices: list[pd.DataFrame | np.ndarray | Tensor],
+        encoder: sp.LabelEncoder | None = None,
+    ) -> None:
+        init_shape = next(iter(matrices)).shape
+        self.n_classes: int = init_shape[0]
+        self.encoder: sp.LabelEncoder | None = encoder
+        self.matrices: list[pd.DataFrame] = []
+        for i, m in enumerate(matrices):
+            self._add_cm(m, i)
+
+    def _add_cm(self, m: pd.DataFrame | np.ndarray | Tensor, i):
+        if m.shape[0] != m.shape[1]:
+            raise ValueError(f"the {i}th confusion matrix is not square!")
+        elif m.shape[0] != self.n_classes:
+            raise ValueError(
+                f"the {i} confusion matrix does not match the shape of the other matrices!"
+            )
+        if isinstance(m, pd.DataFrame):
+            self.matrices.append(m)
+        else:
+            self.matrices.append(ConfusionMatrices.as_df(m, self.encoder))
+
+    def add_cms(self, matrices: Sequence | pd.DataFrame):
+        if not isinstance(matrices, Sequence):
+            self._add_cm(matrices, 0)
+        else:
+            _ = [self._add_cm(m, i) for m, i in enumerate(matrices)]
+
+    def mean_correctness(self) -> pd.DataFrame:
+        dfs = pd.concat(
+            [
+                ConfusionMatrices.correctness(m).loc[
+                    :, ["label", "true_positives", "total_count"]
+                ]
+                for m in self.matrices
+            ]
+        )
+        agg = dfs.groupby("label").agg("sum").reset_index()
+        agg["accuracy"] = agg["true_positives"] / agg["total_count"]
+        agg["label_prop"] = agg["total_count"] / agg["total_count"].sum()
+        return agg
+
+    def std_correctness(self) -> pd.DataFrame:
+        dfs = pd.concat(
+            [
+                ConfusionMatrices.correctness(m).loc[:, ["label", "true_positives"]]
+                for m in self.matrices
+            ]
+        )
+        agg = dfs.groupby("label").agg("std").reset_index()
+        return agg
+
+    def mean(self) -> pd.DataFrame:
+        """Return a single confusion matrix computed by averaging over all matrices"""
+        return reduce(lambda x, y: x + y, self.matrices)
+
+    @staticmethod
+    def as_df(
+        cm: Tensor | np.ndarray, encoder: sp.LabelEncoder | None = None
+    ) -> pd.DataFrame:
+        n_classes = cm.shape[0]
+        labels = [i for i in range(n_classes)]
+        labels = encoder.inverse_transform(labels) if encoder is not None else labels
+        if isinstance(cm, Tensor):
+            cm = cm.numpy()
+        return pd.DataFrame(cm, columns=labels, index=labels)
+
+    @staticmethod
+    def correctness(cm: pd.DataFrame) -> pd.DataFrame:
+        """Report the count of correct predictions for individual
+        labels in confusion matrix `cm`, as well as accuracy
+        Columns in `cm` are taken to be predictions, rows are truth
+        """
+        if cm.shape[0] != cm.shape[1]:
+            raise ValueError("Given confusion matrix is not square!")
+        total_counts = cm.sum(axis=1)
+        tp = np.diag(cm)
+        result = pd.DataFrame(
+            {
+                "label": list(cm.index),
+                "true_positives": tp,
+                "accuracy": tp / total_counts,
+                "total_count": total_counts,
+                "label_prop": total_counts / total_counts.sum(),
+            }
+        )
+        return pd.DataFrame(result).reset_index(drop=True)
+
+
+def format_cms(metric_dcts: list[dict], encoder: sp.LabelEncoder | None = None):
+    """Format the confusion matrix results from a list of `multiclass_all_metrics`"""
+    tasks = next(iter(metric_dcts)).keys()
+    cms = []
+    for dct in metric_dcts:
+        for task in tasks:
+            cm = dct[task]["cm"]
+            cms.append(cm)
+            cm_df: pd.DataFrame = ConfusionMatrices.as_df(cm, encoder)
+            # cm_metrics =
+    return {"label_metrics": [], "average": []}
