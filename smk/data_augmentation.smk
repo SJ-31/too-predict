135f4399b59f44b88da285f0b357106a6771ee14
Shann Chongwattanankul <shanj3131@gmail.com>
2025-08-26 11:33:59 +0700

    chore: fix output inconsistencies in data augmentation

--- smk/data_augmentation.smk
+++ smk/data_augmentation.smk
@@ -2,6 +2,7 @@ include: "Snakefile"
 
 
 import os
+from too_predict.imbalance import TORCH_METHODS
 
 # Workflow to compare the effects of different data augmentation schemes on
 # classification performance
@@ -17,6 +18,9 @@ subsets = da_config["subsets"]
 
 if config["test"]:
     subsets = {"test_subset": {"tumor_type": {".": "contains"}}}
+    config["dl"]["trainer"]["accelerator"] = "cpu"
+    config["dl"]["trainer"]["max_epochs"] = 2
+    config["dl"]["trainer"]["log_every_n_steps"] = 1
 
 dataset_dirs = expand("{store}/{s}", store=store, s=subsets.keys())
 result_dirs = expand("{out}/{s}", out=out, s=subsets.keys())
@@ -27,22 +31,32 @@ rule all:
         csv=f"{out}/all_results.csv",
 
 
+gd_results_dict = {
+    "train": expand("{d}/{a}_train.h5ad", d=dataset_dirs, a=augmentations),
+    "test": expand("{d}/test.h5ad", d=dataset_dirs),
+    "dirs": [directory(d) for d in dataset_dirs],
+}
+if len((torch_augmentations := TORCH_METHODS) & set(augmentations)) > 1:
+    gd_results_dict["lightning_logs"] = expand(
+        "{out}/{s}/{m}_log", out=out, s=subsets.keys(), m=torch_augmentations
+    )
+
+
 rule generate_datasets:
     "Filter dataset, generate synthetic data for each subset and save to storage"
     params:
         store=store,
+        result_dir=out,
         subsets=subsets,
     output:
-        expand("{d}/{a}_train.h5ad", d=dataset_dirs, a=augmentations),
-        expand("{d}/test.h5ad", d=dataset_dirs),
-        dirs=[directory(d) for d in dataset_dirs],
+        **gd_results_dict,
     script:
         "scripts/data_augmentation.py"
 
 
 rule evaluate:
     input:
-        rules.generate_datasets.output.dirs,
+        rules.generate_datasets.output.train,
     params:
         outdir=out,
     output:
--- smk/scripts/data_augmentation.py
+++ smk/scripts/data_augmentation.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import anndata as ad
 import numpy as np
 import pandas as pd
+import sklearn.preprocessing as sp
 import too_predict.deep.evaluation as d_ev
 import too_predict.deep.torch_utils as d_ut
 import too_predict.model as tm
@@ -41,11 +42,13 @@ def get_subset_from_yaml(adata: ad.AnnData, spec: dict) -> ad.AnnData:
 
 
 # You could do this all in `holdout`, but don't want to read everything in all at once
-def evaluate(name: str, train: ad.AnnData, test, validation):
+def evaluate(
+    name: str, train: ad.AnnData, test, validation, encoders: dict[str, sp.LabelEncoder]
+):
     result: dict
     if not USE_TORCH:
         model = tm.PredBase(tm.XGBEstimator())
-        result = train_test_wrapper(
+        result, _ = train_test_wrapper(
             model=model,
             maybe_split=(train, test),
             label_col=LABEL_COL,
@@ -134,39 +137,60 @@ if smk.rule == "generate_datasets":
 
 # ** Evaluate
 if smk.rule == "evaluate":
+    # TODO: need to update this to use shared encoders, or the metrics won't be correct
+    encoders = d_ut.AnnDataset.fit_encoders(
+        ut.training_data_internal(backed=True, only_obs=True)
+        if not TEST
+        else ut.training_data_internal_test(),
+        to_encode=[LABEL_COL],
+    )
     wanted_metrics = ["acc", "kappa", "auroc", "aupr"]
     results = {"dataset": [], "augmentation": []}
     results.update({w: [] for w in wanted_metrics})
     dfs = []
     outdir: Path = Path(smk.params["outdir"])
-    for dir in smk.input:
-        path: Path = Path(str(dir))
-        subset_name = path.stem
-        test = ad.read_h5ad(path.joinpath("test.h5ad"))
+    subset_dirs = {Path(p).parent for p in smk.input}
+    subset2train: dict = {
+        p: [
+            Path(train_file) for train_file in smk.input if Path(train_file).parent == p
+        ]
+        for p in subset_dirs
+    }
+
+    for subset_path, train_paths in subset2train.items():
+        subset_name = subset_path.stem
+        test = ad.read_h5ad(subset_path.joinpath("test.h5ad"))
         cur_outdir = outdir.joinpath(subset_name)
         if USE_TORCH:
-            test = d_ut.AnnDataset(test, to_encode=LABEL_COL)
-        for adata_path in path.glob(pattern="*_train.h5ad"):
-            name = adata_path.stem.replace("_train", "")
+            test = d_ut.AnnDataset(test, to_encode=LABEL_COL, encoders=encoders)
+        for train_path in train_paths:
+            name = train_path.stem.replace("_train", "")
             cur_outfile = cur_outdir.joinpath(f"{name}_result.csv")
             if cur_outfile.exists():
                 df = pd.read_csv(cur_outfile)
             else:
-                adata = ad.read_h5ad(adata_path)
+                adata = ad.read_h5ad(train_path)
                 train, valid = ut.train_test_split_ad(
                     adata,
                     random_state=smk.config["random_state"],
                     test_size=0.1,
                 )
+                if USE_TORCH:
+                    train, valid = (
+                        d_ut.AnnDataset(train, to_encode=LABEL_COL, encoders=encoders),
+                        d_ut.AnnDataset(valid, to_encode=LABEL_COL, encoders=encoders),
+                    )
                 current = results.copy()
                 current["dataset"].append(subset_name)
                 current["augmentation"].append(name)
-                metrics = evaluate(name, train=train, test=test, validation=valid)
+                metrics = evaluate(
+                    name, train=train, test=test, validation=valid, encoders=encoders
+                )
                 for m in wanted_metrics:
                     current[m].append(metrics[m])
                 df = pd.DataFrame(current)
                 if USE_TORCH:
-                    df = d_ut.tensor_cols_to_float(pd.DataFrame(results))
+                    df = d_ut.tensor_cols_to_float(pd.DataFrame(current))
                 df.to_csv(cur_outfile, index=False)
             dfs.append(df)
     df = pd.concat(dfs)
